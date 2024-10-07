d935a61ca1af6143d68f56aa25e89cd4259a653c
명훈 <s23065@gsm.hs.kr>
2024-10-07 20:18:30 +0900

    :green_heart: :: Fix CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 
 dependencies {
 
-    implementation(libs.andㅇㅇroidx.core.ktx)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))

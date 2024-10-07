301a2bd12814a2df42921eaf6858c02c8f802700
명훈 <s23065@gsm.hs.kr>
2024-10-07 22:46:36 +0900

    :memo: :: Update settings.gradle.kts File(:rootProject)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,20 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
 rootProject.name = "Expo-Android"
 include(":app")
- 

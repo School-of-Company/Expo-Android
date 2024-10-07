c209e7ba6fdad5b7d2e1345f03aeca41536412a7
명훈 <s23065@gsm.hs.kr>
2024-10-07 18:23:19 +0900

    :memo: :: Chore build.gradle.kts(root project) File

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.gms.google.service) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
 }

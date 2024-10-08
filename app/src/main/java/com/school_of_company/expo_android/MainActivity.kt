4dbe7de7bd67f4c7325d7004fbb27453a4177671
명훈 <s23065@gsm.hs.kr>
2024-10-08 12:30:27 +0900

    :memo: :: Add @AndroidEntryPoint Annotation - MainActivity

--- app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
+++ app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.school_of_company.expo_android.ui.theme.ExpoAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

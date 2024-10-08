e674f62ed843b73d2727672bf040eb452e7b8894
명훈 <s23065@gsm.hs.kr>
2024-10-08 09:34:28 +0900

    :recycle: :: Apply build-logic -> build.gradle.kts(:core:common)

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("expo.android.core")
+    id("expo.android.hilt")
+}
+
+android {
+    namespace = "com.school_of_company.common"
+}
+
+dependencies {
+    // Add Implementation
+}

874595542ce884d862dff6ca62f0846dcceff47f
명훈 <s23065@gsm.hs.kr>
2024-10-08 14:18:03 +0900

    :sparkles: :: Create RemoteDataSourceModule

---
+++ core/network/src/main/java/com/school_of_company/network/di/RemoteDataSourceModule.kt
@@ -0,0 +1,11 @@
+package com.school_of_company.network.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RemoteDataSourceModule {
+    // todo : Add Bind - RemoteDataSource Elements
+}

117aa663fcd17f116d11f05eb818ac6c08a2d30f
명훈 <s23065@gsm.hs.kr>
2024-10-08 15:54:17 +0900

    :recycle: :: Refactor errorHandling Flexible

--- core/common/src/main/java/com/school_of_company/common/errorHandling/errorHandling.kt
+++ core/common/src/main/java/com/school_of_company/common/errorHandling/errorHandling.kt
@@ -1,23 +1,33 @@
 package com.school_of_company.common.errorHandling
 
-import com.school_of_company.common.action.*
 import com.school_of_company.common.exception.*
 
-fun Throwable.errorHandling(unKnownException: () -> Unit = { unKnownExceptionAction() }) {
-    errorHandlers[this::class.java]?.invoke() ?: unKnownException()
+fun Throwable.errorHandling(
+    badRequestAction: () -> Unit = {},
+    unauthorizedAction: () -> Unit = {},
+    forbiddenAction: () -> Unit = {},
+    notFoundAction: () -> Unit = {},
+    notAcceptableAction: () -> Unit = {},
+    timeOutAction: () -> Unit = {},
+    conflictAction: () -> Unit = {},
+    tooManyRequestAction: () -> Unit = {},
+    serverAction: () -> Unit = {},
+    noInternetAction: () -> Unit = {},
+    otherHttpAction: () -> Unit = {},
+    unknownAction: () -> Unit = {},
+) {
+    when (this) {
+        is BadRequestException -> badRequestAction()
+        is UnauthorizedException, is TokenExpirationException -> unauthorizedAction()
+        is ForBiddenException -> forbiddenAction()
+        is NotFoundException -> notFoundAction()
+        is NotAcceptableException -> notAcceptableAction()
+        is TimeOutException -> timeOutAction()
+        is ConflictException -> conflictAction()
+        is TooManyRequestException -> tooManyRequestAction()
+        is ServerException -> serverAction()
+        is NoInternetException -> noInternetAction()
+        is OtherHttpException -> otherHttpAction()
+        else -> unknownAction()
+    }
 }
-
-private val errorHandlers = mapOf<Class<out Throwable>, () -> Unit> (
-    BadRequestException::class.java to { badRequestExceptionAction() },
-    UnauthorizedException::class.java to { unauthorizedExceptionAction() },
-    TokenExpirationException::class.java to { tokenExpirationExceptionAction() },
-    ForBiddenException::class.java to { forbiddenExceptionAction() },
-    NotFoundException::class.java to { notFoundExceptionAction() },
-    NotAcceptableException::class.java to { notAcceptableExceptionAction() },
-    TimeOutException::class.java to { timeOutExceptionAction() },
-    ConflictException::class.java to { conflictExceptionAction() },
-    TooManyRequestException::class.java to { tooManyRequestExceptionAction() },
-    ServerException::class.java to { serverExceptionAction() },
-    NoInternetException::class.java to { noInternetExceptionAction() },
-    OtherHttpException::class.java to { otherHttpExceptionAction() },
-)

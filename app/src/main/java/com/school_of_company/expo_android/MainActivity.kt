05d2a70b8b820c6e7eac94cd91af29527e5bf235
명훈 <s23065@gsm.hs.kr>
2024-10-10 14:51:15 +0900

    :memo: :: Apply @ExpoPreviews && Modify Greeting Composable Code

--- app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
+++ app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.school_of_company.expo_android.ui.theme.ExpoAndroidTheme
+import com.school_of_company.ui.preview.ExpoPreviews
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,13 +37,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White)
+    )
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
     )
 }
 
-@Preview(showBackground = true)
+@ExpoPreviews
 @Composable
 fun GreetingPreview() {
     ExpoAndroidTheme {

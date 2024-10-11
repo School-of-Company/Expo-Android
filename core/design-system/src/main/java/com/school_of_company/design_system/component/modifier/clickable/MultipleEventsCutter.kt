ba2cc419172bbbcdfa679f92477a0a72346ecb99
명훈 <s23065@gsm.hs.kr>
2024-10-11 15:43:50 +0900

    :sparkles: :: Create MultipleEventsCutter

---
+++ core/design-system/src/main/java/com/school_of_company/design_system/component/modifier/clickable/MultipleEventsCutter.kt
@@ -0,0 +1,24 @@
+package com.school_of_company.design_system.component.modifier.clickable
+
+internal interface MultipleEventsCutter {
+    fun processEvent(event: () -> Unit)
+
+    companion object
+}
+
+internal fun MultipleEventsCutter.Companion.get(intervalMs: Long) : MultipleEventsCutter =
+    MultipleEventsCutterImpl(intervalMs = intervalMs)
+
+private class MultipleEventsCutterImpl(private val intervalMs: Long) : MultipleEventsCutter {
+    private val now: Long
+        get() = System.currentTimeMillis()
+
+    private var lastEventTimeMs: Long = 0L
+
+    override fun processEvent(event: () -> Unit) {
+        if (now - lastEventTimeMs >= intervalMs) {
+            event.invoke()
+        }
+        lastEventTimeMs = now
+    }
+}

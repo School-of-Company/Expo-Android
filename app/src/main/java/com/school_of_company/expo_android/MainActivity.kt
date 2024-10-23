dff908f912ef185a0497da520d58ca2af1fd44de
명훈 <s23065@gsm.hs.kr>
2024-10-23 17:07:52 +0900

    :memo: :: Add installSplashScreen() Function

--- app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
+++ app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             // A surface container using the 'background' color from the theme

7ea2700f4a22b36863953cc05dd6b1145f1a3c5c
명훈 <s23065@gsm.hs.kr>
2024-10-23 18:57:35 +0900

    :sparkles: :: Create TopLevelDestination

---
+++ app/src/main/java/com/school_of_company/expo_android/navigation/TopLevelDestination.kt
@@ -0,0 +1,25 @@
+package com.school_of_company.expo_android.navigation
+
+import com.school_of_company.design_system.R
+
+enum class TopLevelDestination(
+    unSelectedIcon: Int,
+    iconText: String
+) {
+    HOME(
+        unSelectedIcon = R.drawable.ic_house,
+        iconText = "홈"
+    ),
+    ROSTER(
+        unSelectedIcon = R.drawable.ic_append,
+        iconText = "명단 관리"
+    ),
+    EXPO(
+        unSelectedIcon = R.drawable.ic_expo,
+        iconText = "박람회 생성"
+    ),
+    PROFILE(
+        unSelectedIcon = R.drawable.ic_user,
+        iconText = "프로필"
+    )
+}

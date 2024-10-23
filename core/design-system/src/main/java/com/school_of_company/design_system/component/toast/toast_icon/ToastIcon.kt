7526d2a3a3004cfc661d42781272cbf59f218aba
명훈 <s23065@gsm.hs.kr>
2024-10-24 08:59:05 +0900

    :sparkles: :: Create ToastIcon(Drawable Resource)

---
+++ core/design-system/src/main/java/com/school_of_company/design_system/component/toast/toast_icon/ToastIcon.kt
@@ -0,0 +1,8 @@
+package com.school_of_company.design_system.component.toast.toast_icon
+
+import com.school_of_company.design_system.R
+
+object ToastIcon {
+    val Success = R.drawable.ic_check
+    val FAIL = R.drawable.ic_x
+}

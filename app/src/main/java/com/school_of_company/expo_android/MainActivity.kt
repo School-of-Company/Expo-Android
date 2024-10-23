f164e62938e4d8fdbf7d9e2ad2d0eca751182356
명훈 <s23065@gsm.hs.kr>
2024-10-23 20:05:27 +0900

    :memo: :: Apply Before Commit

--- app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
+++ app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.school_of_company.expo_android.ui.ExpoApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         installSplashScreen()
+
         enableEdgeToEdge()
+
         setContent {
-            // A surface container using the 'background' color from the theme
+            CompositionLocalProvider {
+                ExpoApp(windowSizeClass = calculateWindowSizeClass(activity = this))
+            }
         }
     }
 }

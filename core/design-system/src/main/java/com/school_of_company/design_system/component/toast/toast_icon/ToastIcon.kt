324bf86db90835aeeb4c8fb317dba1e0e4f11dd6
명훈 <s23065@gsm.hs.kr>
2024-10-24 09:18:46 +0900

    :rewind: :: Rewind Changed

--- core/design-system/src/main/java/com/school_of_company/design_system/component/toast/toast_icon/ToastIcon.kt
+++
@@ -1,8 +0,0 @@
-package com.school_of_company.design_system.component.toast.toast_icon
-
-import com.school_of_company.design_system.R
-
-object ToastIcon {
-    val Success = R.drawable.ic_check
-    val FAIL = R.drawable.ic_x
-}

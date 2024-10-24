2c22727ffe825f5ea75588a6e4db228dfaadf279
명훈 <s23065@gsm.hs.kr>
2024-10-24 09:35:10 +0900

    :recycle: :: Change Naming

--- core/ui/src/main/java/com/school_of_company/ui/toast/ExpoToast.kt
+++ core/ui/src/main/java/com/school_of_company/ui/toast/ExpoToast.kt
@@ -3,7 +3,7 @@ package com.school_of_company.ui.toast
 import android.content.Context
 import android.widget.Toast
 
-fun ExpoToast(
+fun makeToast(
     context: Context,
     toastMessage: String,
     duration: Int = Toast.LENGTH_SHORT

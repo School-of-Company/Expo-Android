d17ff8b315c9ff7aee1b7fdcc9b8d1d9fb2b7c76
명훈 <s23065@gsm.hs.kr>
2024-10-24 20:45:56 +0900

    :recycle: :: Rename AdminTokenResponse

--- core/network/src/main/java/com/school_of_company/network/dto/auth/response/AdminTokenResponse.kt
+++ core/network/src/main/java/com/school_of_company/network/dto/auth/response/AdminTokenResponse.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class AdminRefreshTokenResponse(
+data class AdminTokenResponse(
     @Json(name = "accessToken") val accessToken: String,
     @Json(name = "refreshToken") val refreshToken: String,
     @Json(name = "accessTokenExpiresIn") val accessTokenExpiresIn: String,

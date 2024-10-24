69134d08cfcf4d8ce5423fb625292f52acf7eb1e
명훈 <s23065@gsm.hs.kr>
2024-10-24 21:21:37 +0900

    :sparkles: :: Create AuthRepositoryImpl

---
+++ core/data/src/main/java/com/school_of_company/data/repository/auth/AuthRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.school_of_company.data.repository.auth
+
+import com.school_of_company.model.model.auth.AdminTokenResponseModel
+import com.school_of_company.model.param.auth.AdminSignInRequestParam
+import com.school_of_company.model.param.auth.AdminSignUpRequestParam
+import com.school_of_company.network.datasource.auth.AuthDataSource
+import com.school_of_company.network.mapper.auth.request.toDto
+import com.school_of_company.network.mapper.auth.response.toModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+    private val datasource: AuthDataSource
+) : AuthRepository {
+    override fun adminSignUp(body: AdminSignUpRequestParam): Flow<Unit> {
+        return datasource.adminSignUp(body = body.toDto())
+    }
+
+    override fun adminSignIn(body: AdminSignInRequestParam): Flow<AdminTokenResponseModel> {
+        return datasource.adminSignIn(
+            body = body.toDto()
+        ).transform { reponse ->
+            emit(reponse.toModel())
+        }
+    }
+
+    override fun adminTokenRefresh(refreshToken: String): Flow<AdminTokenResponseModel> {
+        return datasource.adminTokenRefresh(
+            refreshToken = refreshToken
+        ).transform { reponse ->
+            emit(reponse.toModel())
+        }
+    }
+
+    override fun adminLogout(): Flow<Unit> {
+        return datasource.adminLogout()
+    }
+}

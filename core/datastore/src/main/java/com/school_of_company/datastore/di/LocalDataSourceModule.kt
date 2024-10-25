53b8a1db8ac537b671beb63261ad22b36ec10b73
명훈 <s23065@gsm.hs.kr>
2024-10-25 21:25:52 +0900

    :memo: :: Apply LocalDataSourceModule

--- core/datastore/src/main/java/com/school_of_company/datastore/di/LocalDataSourceModule.kt
+++ core/datastore/src/main/java/com/school_of_company/datastore/di/LocalDataSourceModule.kt
@@ -1,5 +1,8 @@
 package com.school_of_company.datastore.di
 
+import com.school_of_company.datastore.datasource.AuthTokenDataSource
+import com.school_of_company.datastore.datasource.AuthTokenDataSourceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,5 +10,9 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class LocalDataSourceModule {
-    // todo : Add Bind - LocalDataSource Elements
+
+    @Binds
+    abstract fun bindAuthTokenDataSource(
+        authTokenDataSourceImpl: AuthTokenDataSourceImpl
+    ): AuthTokenDataSource
 }

f8075f08d4fef11f07a724856df5aa3cc5019c96
명훈 <s23065@gsm.hs.kr>
2024-10-28 15:13:17 +0900

    :memo: :: Add Log

--- core/domain/src/main/java/com/school_of_company/domain/usecase/sms/SmsSignUpCertificationNumberCertificationRequestUseCase.kt
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/sms/SmsSignUpCertificationNumberCertificationRequestUseCase.kt
@@ -1,5 +1,6 @@
 package com.school_of_company.domain.usecase.sms
 
+import android.util.Log
 import com.school_of_company.data.repository.sms.SmsRepository
 import com.school_of_company.model.param.sms.SmsSignUpCertificationNumberCertificationRequestParam
 import javax.inject.Inject
@@ -8,6 +9,7 @@ class SmsSignUpCertificationNumberCertificationRequestUseCase @Inject constructo
     private val repository: SmsRepository
 ) {
     operator fun invoke(body: SmsSignUpCertificationNumberCertificationRequestParam) = runCatching {
+        Log.d("CertificationUseCase", "SmsSignUpCertificationNumberCertificationRequestUseCase called") // 호출 로그 추가
         repository.smsSignUpCertificationNumberCertification(body = body)
     }
 }

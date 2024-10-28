bf30fde290dcae2dc9c998649e22d64a2b546a37
명훈 <s23065@gsm.hs.kr>
2024-10-28 18:25:06 +0900

    :recycle: :: @Body -> @Query

--- core/network/src/main/java/com/school_of_company/network/api/SmsAPI.kt
+++ core/network/src/main/java/com/school_of_company/network/api/SmsAPI.kt
@@ -1,10 +1,10 @@
 package com.school_of_company.network.api
 
-import com.school_of_company.network.dto.sms.request.SmsSignUpCertificationNumberCertificationRequest
 import com.school_of_company.network.dto.sms.request.SmsSignUpCertificationNumberSendRequest
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface SmsAPI {
 
@@ -15,6 +15,7 @@ interface SmsAPI {
 
     @GET("/sms")
     suspend fun smsSignUpCertificationNumberCertification(
-        @Body body: SmsSignUpCertificationNumberCertificationRequest
+        @Query("phoneNumber") phoneNumber: String,
+        @Query("code") code: String
     )
 }

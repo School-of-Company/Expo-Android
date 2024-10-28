d1d451be2cd7c72e3356e1f1a81c9fdcc8308a2e
명훈 <s23065@gsm.hs.kr>
2024-10-28 15:16:12 +0900

    :sparkles: :: Apply Certification Functions

--- feature/signup/src/main/java/com/school_of_company/signup/viewmodel/SignUpViewModel.kt
+++ feature/signup/src/main/java/com/school_of_company/signup/viewmodel/SignUpViewModel.kt
@@ -1,5 +1,6 @@
 package com.school_of_company.signup.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,8 +8,14 @@ import com.school_of_company.common.network.errorHandling
 import com.school_of_company.common.regex.checkEmailRegex
 import com.school_of_company.common.regex.checkPasswordRegex
 import com.school_of_company.domain.usecase.auth.AdminSignUpRequestUseCase
+import com.school_of_company.domain.usecase.sms.SmsSignUpCertificationNumberCertificationRequestUseCase
+import com.school_of_company.domain.usecase.sms.SmsSignUpCertificationNumberSendRequestUseCase
 import com.school_of_company.model.param.auth.AdminSignUpRequestParam
+import com.school_of_company.model.param.sms.SmsSignUpCertificationNumberCertificationRequestParam
+import com.school_of_company.model.param.sms.SmsSignUpCertificationNumberSendRequestParam
 import com.school_of_company.signup.viewmodel.uistate.SignUpUiState
+import com.school_of_company.signup.viewmodel.uistate.SmsSignUpCertificationCodeUiState
+import com.school_of_company.signup.viewmodel.uistate.SmsSignUpCertificationSendCodeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -20,7 +27,9 @@ import javax.inject.Inject
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
-    private val signUpRequestUseCase: AdminSignUpRequestUseCase
+    private val signUpRequestUseCase: AdminSignUpRequestUseCase,
+    private val smsSignUpCertificationNumberSendRequestUseCase: SmsSignUpCertificationNumberSendRequestUseCase,
+    private val smsSignUpCertificationNumberCertificationRequestUseCase: SmsSignUpCertificationNumberCertificationRequestUseCase
 ) : ViewModel() {
     companion object {
         private const val NAME = "nickname"
@@ -29,9 +38,16 @@ class SignUpViewModel @Inject constructor(
         private const val PASSWORD = "password"
         private const val RE_PASSWORD = "rePassword"
         private const val PHONE_NUMBER = "phoneNumber"
+        private const val CERTIFICATION_NUMBER = "certificationNumber"
     }
     private var _signUpUiState = MutableStateFlow<SignUpUiState>(SignUpUiState.Loading)
     internal val signUpUiState = _signUpUiState.asStateFlow()
+
+    private var _smsSignUpCertificationSendCodeUiState = MutableStateFlow<SmsSignUpCertificationSendCodeUiState>(SmsSignUpCertificationSendCodeUiState.Loading)
+    internal val smsSignUpCertificationSendCodeUiState = _smsSignUpCertificationSendCodeUiState.asStateFlow()
+
+    private var _smsSignUpCertificationCodeUiState = MutableStateFlow<SmsSignUpCertificationCodeUiState>(SmsSignUpCertificationCodeUiState.Loading)
+    internal val smsSignUpCertificationCodeUiState = _smsSignUpCertificationCodeUiState.asStateFlow()
 
     internal var name = savedStateHandle.getStateFlow(key = NAME, initialValue = "")
 
@@ -45,7 +61,11 @@ class SignUpViewModel @Inject constructor(
 
     internal var phoneNumber = savedStateHandle.getStateFlow(key = PHONE_NUMBER, initialValue = "")
 
+    internal var certificationNumber = savedStateHandle.getStateFlow(key = CERTIFICATION_NUMBER, initialValue = "")
+
     private var _isError = MutableStateFlow(false)
+
+    private var _isCodeError = MutableStateFlow(false)
 
     private var _isEmailValidError = MutableStateFlow(false)
     internal val isEmailValidError: StateFlow<Boolean> = _isEmailValidError.asStateFlow()
@@ -57,6 +77,13 @@ class SignUpViewModel @Inject constructor(
     internal val isPasswordMismatchError: StateFlow<Boolean> = _isPasswordMismatchError.asStateFlow()
 
     private var _isDuplicateAccountError = MutableStateFlow(false)
+
+    private var _isCertificationCodeValid = MutableStateFlow(false)
+    internal val isCertificationCodeValid: StateFlow<Boolean> = _isCertificationCodeValid.asStateFlow()
+
+    internal fun setCodeError(value: Boolean) {
+        _isCodeError.value = value
+    }
 
     internal fun setError(value: Boolean) {
         _isError.value = value
@@ -76,6 +103,10 @@ class SignUpViewModel @Inject constructor(
 
     internal fun setDuplicateAccountError(value: Boolean) {
         _isDuplicateAccountError.value = value
+    }
+
+    internal fun setCertificationCodeValid(value: Boolean) {
+        _isCertificationCodeValid.value = value
     }
 
     internal fun initSignUp() {
@@ -123,6 +154,41 @@ class SignUpViewModel @Inject constructor(
         }
     }
 
+    internal fun certificationCode(body: SmsSignUpCertificationNumberCertificationRequestParam) = viewModelScope.launch {
+        Log.d("SignUpViewModel", "Certification Code function called") // 로그 추가
+        setCodeError(false)
+        setCertificationCodeValid(false)
+        smsSignUpCertificationNumberCertificationRequestUseCase(body = body)
+            .onSuccess {
+                Log.d("SignUpViewModel", "Certification code request successful") // 성공 로그
+                it.catch { remoteError ->
+                    Log.e("SignUpViewModel", "Error caught in flow", remoteError)
+                    _smsSignUpCertificationCodeUiState.value = SmsSignUpCertificationCodeUiState.Error(remoteError)
+                }.collect {
+                    _smsSignUpCertificationCodeUiState.value = SmsSignUpCertificationCodeUiState.Success
+                    setCodeError(true)
+                }
+            }
+            .onFailure { error ->
+                Log.e("SignUpViewModel", "Certification code request failed", error) // 실패 로그
+                _smsSignUpCertificationCodeUiState.value = SmsSignUpCertificationCodeUiState.Error(error)
+                setCodeError(true)
+            }
+    }
+
+    internal fun sendCertificationCode(body: SmsSignUpCertificationNumberSendRequestParam) = viewModelScope.launch {
+        _smsSignUpCertificationSendCodeUiState.value = SmsSignUpCertificationSendCodeUiState.Loading
+        smsSignUpCertificationNumberSendRequestUseCase(body = body)
+            .onSuccess {
+                it.catch { remoteError ->
+                    _smsSignUpCertificationSendCodeUiState.value = SmsSignUpCertificationSendCodeUiState.Error(remoteError)
+                }.collect { _smsSignUpCertificationSendCodeUiState.value = SmsSignUpCertificationSendCodeUiState.Success }
+            }
+            .onFailure { error ->
+                _smsSignUpCertificationSendCodeUiState.value = SmsSignUpCertificationSendCodeUiState.Error(error)
+            }
+    }
+
     internal fun onNameChange(value: String) { savedStateHandle[NAME] = value }
 
     internal fun onNicknameChange(value: String) { savedStateHandle[NICKNAME] = value }
@@ -134,4 +200,6 @@ class SignUpViewModel @Inject constructor(
     internal fun onRePasswordChange(value: String) { savedStateHandle[RE_PASSWORD] = value }
 
     internal fun onPhoneNumberChange(value: String) { savedStateHandle[PHONE_NUMBER] = value }
+
+    internal fun onCertificationNumberChange(value: String) { savedStateHandle[CERTIFICATION_NUMBER] = value }
 }

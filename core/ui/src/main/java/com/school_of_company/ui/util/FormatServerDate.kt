7f0ddb479995a35073682b74a8b51343af5c1ebf
명훈 <s23065@gsm.hs.kr>
2024-11-04 08:43:36 +0900

    :recycle: :: LocalDate -> MonthDate

--- core/ui/src/main/java/com/school_of_company/ui/util/FormatServerDate.kt
+++ core/ui/src/main/java/com/school_of_company/ui/util/FormatServerDate.kt
@@ -1,9 +1,9 @@
 package com.school_of_company.ui.util
 
-import java.time.LocalDate
+import java.time.MonthDay
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 fun String.formatServerDate(): String =
-    LocalDate.parse(this, DateTimeFormatter.ofPattern("MM-dd"))
+    MonthDay.parse(this, DateTimeFormatter.ofPattern("MM-dd"))
         .format(DateTimeFormatter.ofPattern("MM월 dd일", Locale.getDefault()))

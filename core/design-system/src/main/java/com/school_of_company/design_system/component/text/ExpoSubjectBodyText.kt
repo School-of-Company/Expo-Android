132f8cc68d7bcce3f114259f286e31e1a7690723
명훈 <s23065@gsm.hs.kr>
2024-11-05 15:53:32 +0900

    :recycle: :: Text OverFlow(MaxLine 1)

--- core/design-system/src/main/java/com/school_of_company/design_system/component/text/ExpoSubjectBodyText.kt
+++ core/design-system/src/main/java/com/school_of_company/design_system/component/text/ExpoSubjectBodyText.kt
@@ -1,8 +1,8 @@
 package com.school_of_company.design_system.component.text
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
 import com.school_of_company.design_system.theme.ExpoAndroidTheme
 
 @Composable
@@ -12,6 +12,8 @@ fun ExpoSubjectTitleText(subjectText: String) {
             text = subjectText,
             color = colors.black,
             style = typography.bodyBold1,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }

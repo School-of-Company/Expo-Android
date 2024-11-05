e2e15a623d807395ca696f999fd32f67a80e064f
명훈 <s23065@gsm.hs.kr>
2024-11-05 16:05:00 +0900

    :memo: :: Add HomeDetailNavigation

--- feature/home/src/main/java/com/school_of_company/home/navigation/HomeNavigation.kt
+++ feature/home/src/main/java/com/school_of_company/home/navigation/HomeNavigation.kt
@@ -4,12 +4,18 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.school_of_company.home.view.HomeDetailRoute
 import com.school_of_company.home.view.HomeRoute
 
 const val homeRoute = "home_route"
+const val homeDetailRoute=  "home_detail_route"
 
 fun NavController.navigateToHome(navOptions: NavOptions? = null) {
     this.navigate(homeRoute, navOptions)
+}
+
+fun NavController.navigateToHomeDetail(navOptions: NavOptions? = null) {
+    this.navigate(homeDetailRoute, navOptions)
 }
 
 fun NavGraphBuilder.homeScreen(
@@ -19,5 +25,13 @@ fun NavGraphBuilder.homeScreen(
         HomeRoute(
             navigationToDetail = navigationToDetail
         )
+    }
+}
+
+fun NavGraphBuilder.homeDetailScreen(
+    onBackClick: () -> Unit
+) {
+    composable(route = homeDetailRoute) {
+        HomeDetailRoute(onBackClick = onBackClick)
     }
 }

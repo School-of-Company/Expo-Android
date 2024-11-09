3cc9bc6b7b2eb69dc463d17145bd87de3b038299
명훈 <s23065@gsm.hs.kr>
2024-11-10 02:19:44 +0900

    :sparkles: :: Create ExpoMapper(Request, Response)

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/expo/request/ExpoRequestMapper.kt
@@ -0,0 +1,16 @@
+package com.school_of_company.network.mapper.expo.request
+
+import com.school_of_company.model.model.expo.ExpoRequestAndResponseModel
+import com.school_of_company.network.dto.expo.request_response.ExpoRequestAndResponse
+
+fun ExpoRequestAndResponseModel.toDto(): ExpoRequestAndResponse =
+    ExpoRequestAndResponse(
+        title = this.title,
+        description = this.description,
+        startedDay = this.startedDay,
+        finishedDay = this.finishedDay,
+        location = this.location,
+        coverImage = this.coverImage,
+        x = this.x,
+        y = this.y
+    )

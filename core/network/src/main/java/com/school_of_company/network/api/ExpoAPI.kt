593a4dc8ee35ef09b65437c6a3d05ec60feb74b3
명훈 <s23065@gsm.hs.kr>
2024-11-10 02:20:02 +0900

    :sparkles: :: Create ExpoAPI

---
+++ core/network/src/main/java/com/school_of_company/network/api/ExpoAPI.kt
@@ -0,0 +1,28 @@
+package com.school_of_company.network.api
+
+import com.school_of_company.network.dto.expo.request_response.ExpoRequestAndResponse
+import retrofit2.http.*
+
+interface ExpoAPI {
+
+    @GET("/expo/{expo_id}")
+    suspend fun getExpoInformation(
+        @Path("expo_id") expoId: Long
+    ) : ExpoRequestAndResponse
+
+    @POST("/expo")
+    suspend fun registerExpoInformation(
+        @Body body: ExpoRequestAndResponse
+    )
+
+    @PATCH("/expo/{expo_id}")
+    suspend fun modifyExpoInformation(
+        @Path("expo_id") expoId: Long,
+        @Body body: ExpoRequestAndResponse
+    )
+
+    @DELETE("/expo/{expo_id}")
+    suspend fun deleteExpoInformation(
+        @Path("expo_id") expoId: Long
+    )
+}

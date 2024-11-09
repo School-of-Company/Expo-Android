19ef97b457b891e03388b7e2378b169ca01aba13
명훈 <s23065@gsm.hs.kr>
2024-11-10 02:26:12 +0900

    :memo: :: Apply Before Commit -> Modules

--- core/data/src/main/java/com/school_of_company/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/school_of_company/data/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.school_of_company.data.di
 
 import com.school_of_company.data.repository.auth.AuthRepository
 import com.school_of_company.data.repository.auth.AuthRepositoryImpl
+import com.school_of_company.data.repository.expo.ExpoRepository
+import com.school_of_company.data.repository.expo.ExpoRepositoryImpl
 import com.school_of_company.data.repository.sms.SmsRepository
 import com.school_of_company.data.repository.sms.SmsRepositoryImpl
 import dagger.Binds
@@ -22,4 +24,10 @@ abstract class RepositoryModule {
     abstract fun bindSmsRepository(
         smsRepositoryImpl: SmsRepositoryImpl
     ) : SmsRepository
+
+    @Binds
+    abstract fun bindExpoRepository(
+        expoRepositoryImpl: ExpoRepositoryImpl
+    ) : ExpoRepository
+
 }

0a489d42f82f10be0ed70bf4872e2e8091a9d968
명훈 <s23065@gsm.hs.kr>
2024-11-10 02:18:53 +0900

    :sparkles: :: Create ExpoRequestAndResponse

---
+++ core/network/src/main/java/com/school_of_company/network/dto/expo/request_response/ExpoRequestAndResponse.kt
@@ -0,0 +1,16 @@
+package com.school_of_company.network.dto.expo.request_response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ExpoRequestAndResponse(
+    @Json(name = "title") val title: String,
+    @Json(name = "description") val description: String,
+    @Json(name = "startedDay") val startedDay: String, // yyyy-mm-dd
+    @Json(name = "finishedDay") val finishedDay: String, // yyyy-mm-dd
+    @Json(name = "location") val location: String,
+    @Json(name = "coverImage") val coverImage: String,
+    @Json(name = "x") val x: Float,
+    @Json(name = "y") val y: Float
+)

966b25bac9aef60a0575b495248ac13e3e20aee6
명훈 <s23065@gsm.hs.kr>
2024-11-11 23:14:41 +0900

    :memo: :: Add New Repository Element

--- core/data/src/main/java/com/school_of_company/data/repository/expo/ExpoRepository.kt
+++ core/data/src/main/java/com/school_of_company/data/repository/expo/ExpoRepository.kt
@@ -1,9 +1,11 @@
 package com.school_of_company.data.repository.expo
 
+import com.school_of_company.model.entity.expo.ExpoListResponseEntity
 import com.school_of_company.model.model.expo.ExpoRequestAndResponseModel
 import kotlinx.coroutines.flow.Flow
 
 interface ExpoRepository {
+    fun getExpoList() : Flow<List<ExpoListResponseEntity>>
     fun getExpoInformation(expoId: Long) : Flow<ExpoRequestAndResponseModel>
     fun registerExpoInformation(body: ExpoRequestAndResponseModel) : Flow<Unit>
     fun modifyExpoInformation(expoId: Long, body: ExpoRequestAndResponseModel) : Flow<Unit>
--- core/data/src/main/java/com/school_of_company/data/repository/expo/ExpoRepositoryImpl.kt
+++ core/data/src/main/java/com/school_of_company/data/repository/expo/ExpoRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.school_of_company.data.repository.expo
 
+import com.school_of_company.model.entity.expo.ExpoListResponseEntity
 import com.school_of_company.model.model.expo.ExpoRequestAndResponseModel
 import com.school_of_company.network.datasource.expo.ExpoDataSource
 import com.school_of_company.network.mapper.expo.request.toDto
+import com.school_of_company.network.mapper.expo.response.toEntity
 import com.school_of_company.network.mapper.expo.response.toModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
@@ -11,6 +13,12 @@ import javax.inject.Inject
 class ExpoRepositoryImpl @Inject constructor(
     private val dataSource: ExpoDataSource
 ) : ExpoRepository {
+    override fun getExpoList(): Flow<List<ExpoListResponseEntity>> {
+        return dataSource.getExpoList().transform { list ->
+            emit(list.map { it.toEntity() })
+        }
+    }
+
     override fun getExpoInformation(expoId: Long): Flow<ExpoRequestAndResponseModel> {
         return dataSource.getExpoInformation(expoId = expoId).transform { response ->
             emit(response.toModel())

701bd81301c9481d3cbe5fd8e51be8324aa0c27e
명훈 <s23065@gsm.hs.kr>
2024-11-11 23:09:56 +0900

    :sparkles: :: Create ExpoListResponseEntity

---
+++ core/model/src/main/java/com/school_of_company/model/entity/expo/ExpoListResponseEntity.kt
@@ -0,0 +1,9 @@
+package com.school_of_company.model.entity.expo
+
+data class ExpoListResponseEntity(
+    val title: String,
+    val description: String,
+    val startedDay: String, // yyyy-mm-dd
+    val finishedDay: String, // yyyy-mm-dd
+    val coverImage: String,
+)

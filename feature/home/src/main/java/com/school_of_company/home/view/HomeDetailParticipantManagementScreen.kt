ce6fca4aefad539c58b638da5f95d8d5aee08ea4
명훈 <s23065@gsm.hs.kr>
2024-11-13 18:12:03 +0900

    :lipstick: :: Publising HomeDetailParticipantManagementScreen

---
+++ feature/home/src/main/java/com/school_of_company/home/view/HomeDetailParticipantManagementScreen.kt
@@ -0,0 +1,221 @@
+package com.school_of_company.home.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.school_of_company.design_system.component.modifier.clickable.expoClickable
+import com.school_of_company.design_system.component.topbar.ExpoTopBar
+import com.school_of_company.design_system.icon.LeftArrowIcon
+import com.school_of_company.design_system.icon.WarnIcon
+import com.school_of_company.design_system.theme.ExpoAndroidTheme
+import com.school_of_company.home.view.component.HomeDetailParticipantManagementData
+import com.school_of_company.home.view.component.HomeDetailParticipantManagementList
+import com.school_of_company.home.view.component.HomeDetailProgramParticipantData
+import com.school_of_company.home.view.component.HomeDetailProgramParticipantList
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toPersistentList
+
+fun generateParticipantManagementSampleData(): ImmutableList<HomeDetailParticipantManagementData> {
+    return List(20) {
+        HomeDetailParticipantManagementData(
+            name = "이명훈",
+            company = "초등학교",
+            position = "교사",
+            schoolSubject = "컴퓨터공학",
+            checkHere = true,
+            inTime = "12:00",
+            outTime = "13:00"
+        )
+    }.toPersistentList()
+}
+
+@Composable
+internal fun HomeDetailParticipantManagementRoute(
+    onBackClick: () -> Unit
+) {
+    HomeDetailParticipantManagementScreen(
+        onBackClick = onBackClick,
+        participantManagementData = generateParticipantManagementSampleData()
+    )
+}
+
+@Composable
+internal fun HomeDetailParticipantManagementScreen(
+    modifier: Modifier = Modifier,
+    participantManagementData: ImmutableList<HomeDetailParticipantManagementData>,
+    onBackClick: () -> Unit
+) {
+    ExpoAndroidTheme { colors, typography ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color = colors.white)
+                .padding(top = 68.dp)
+        ) {
+            ExpoTopBar(
+                startIcon = {
+                    LeftArrowIcon(
+                        tint = colors.black,
+                        modifier = Modifier.expoClickable { onBackClick() }
+                    )
+                },
+                betweenText = "참가자 관리",
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(28.dp))
+
+            Text(
+                text = "참가자 조회하기",
+                style = typography.bodyBold2,
+                color = colors.black,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+                Row(horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start)) {
+                    WarnIcon(
+                        tint = colors.gray300,
+                        modifier = Modifier.size(16.dp)
+                    )
+
+                    Text(
+                        text = "옆으로 넘겨서 확인해보세요",
+                        style = typography.captionRegular2,
+                        color = colors.gray300
+                    )
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Spacer(
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .width(1.dp)
+                        .height(14.dp)
+                        .background(color = colors.gray100)
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Row(horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start)) {
+                    Text(
+                        text = "참가자 전체 인원",
+                        style = typography.captionRegular2,
+                        color = colors.gray500
+                    )
+
+                    Text(
+                        text = "${participantManagementData.size}명",
+                        style = typography.captionRegular2,
+                        color = colors.main
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Box(
+                modifier = Modifier
+                    .border(
+                        width = 1.dp,
+                        color = colors.gray200
+                    )
+                    .fillMaxWidth()
+                    .padding(
+                        vertical = 16.dp
+                    )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp)
+                        .horizontalScroll(rememberScrollState()),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    Text(
+                        text = "성명",
+                        style = typography.captionBold1,
+                        color = colors.gray600,
+                        modifier = Modifier.width(80.dp)
+                    )
+
+                    Text(
+                        text = "소속",
+                        style = typography.captionBold1,
+                        color = colors.gray600,
+                        modifier = Modifier.width(100.dp)
+                    )
+
+                    Text(
+                        text = "직급",
+                        style = typography.captionBold1,
+                        color = colors.gray600,
+                        modifier = Modifier.width(80.dp)
+                    )
+
+                    Text(
+                        text = "프로그램 선택",
+                        style = typography.captionBold1,
+                        color = colors.gray600,
+                        modifier = Modifier.width(120.dp)
+                    )
+
+                    Text(
+                        text = "출석 여부",
+                        style = typography.captionBold1,
+                        color = colors.gray600,
+                        modifier = Modifier.width(100.dp)
+                    )
+
+                    Text(
+                        text = "입실시간",
+                        style = typography.captionBold1,
+                        color = colors.gray600,
+                        modifier = Modifier.width(80.dp)
+                    )
+
+                    Text(
+                        text = "퇴실시간",
+                        style = typography.captionBold1,
+                        color = colors.gray600,
+                        modifier = Modifier.width(80.dp)
+                    )
+                }
+            }
+
+            HomeDetailParticipantManagementList(item = participantManagementData)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeDetailParticipantManagementScreenPreview() {
+    HomeDetailParticipantManagementScreen(
+        onBackClick = {},
+        participantManagementData = generateParticipantManagementSampleData()
+    )
+}

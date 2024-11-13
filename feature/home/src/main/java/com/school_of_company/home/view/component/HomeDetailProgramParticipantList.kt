b2c97693317e22ed2421a7d21400b31ac6c02cb4
명훈 <s23065@gsm.hs.kr>
2024-11-13 15:13:06 +0900

    :lipstick: :: Publishing HomeDetailProgramparticipantList

---
+++ feature/home/src/main/java/com/school_of_company/home/view/component/HomeDetailProgramParticipantList.kt
@@ -0,0 +1,59 @@
+package com.school_of_company.home.view.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.school_of_company.design_system.theme.ExpoAndroidTheme
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+@Composable
+fun HomeDetailProgramParticipantList(
+    modifier: Modifier = Modifier,
+    item: ImmutableList<HomeDetailProgramParticipantData> = persistentListOf()
+) {
+    ExpoAndroidTheme { colors, _ ->
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color = colors.white)
+                .padding(start = 16.dp)
+        ) {
+            itemsIndexed(item) { index, item ->
+                HomeDetailProgramParticipantListItem(
+                    index = index + 1,
+                    data = item
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeDetailProgramParticipantListPreview() {
+    HomeDetailProgramParticipantList(
+        item = persistentListOf(
+            HomeDetailProgramParticipantData(
+                name = "이명훈",
+                company = "초등학교",
+                position = "교사",
+                schoolSubject = "컴퓨터공학",
+                phone = "010-1234-5678"
+            ),
+            HomeDetailProgramParticipantData(
+                name = "이명훈",
+                company = "초등학교",
+                position = "교사",
+                schoolSubject = "컴퓨터공학",
+                phone = "010-1234-5678"
+            ),
+        )
+    )
+}
---
+++ feature/home/src/main/java/com/school_of_company/home/view/component/HomeDetailProgramParticipantListItem.kt
@@ -0,0 +1,102 @@
+package com.school_of_company.home.view.component
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.school_of_company.design_system.theme.ExpoAndroidTheme
+
+data class HomeDetailProgramParticipantData(
+    val name: String,
+    val company: String,
+    val position: String,
+    val schoolSubject: String,
+    val phone: String
+)
+
+@Composable
+fun HomeDetailProgramParticipantListItem(
+    modifier: Modifier = Modifier,
+    index: Int,
+    data: HomeDetailProgramParticipantData,
+    horizontalScrollState: ScrollState = rememberScrollState()
+) {
+    ExpoAndroidTheme { colors, typography ->
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .horizontalScroll(rememberScrollState()), // 수평 스크롤 가능
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Text(
+                text = index.toString(),
+                style = typography.captionRegular2,
+                color = colors.black,
+                modifier = Modifier.width(20.dp) // 제목과 동일한 너비
+            )
+
+            Text(
+                text = data.name,
+                style = typography.captionRegular2,
+                color = colors.black,
+                modifier = Modifier.width(80.dp) // 제목과 동일한 너비 설정
+            )
+            Text(
+                text = data.company,
+                style = typography.captionRegular2,
+                color = colors.black,
+                modifier = Modifier.width(100.dp) // 제목과 동일한 너비 설정
+            )
+            Text(
+                text = data.position,
+                style = typography.captionRegular2,
+                color = colors.black,
+                modifier = Modifier.width(80.dp) // 제목과 동일한 너비 설정
+            )
+            Text(
+                text = data.schoolSubject,
+                style = typography.captionRegular2,
+                color = colors.black,
+                modifier = Modifier.width(120.dp) // 제목과 동일한 너비 설정
+            )
+            Text(
+                text = data.phone,
+                style = typography.captionRegular2,
+                color = colors.black,
+                modifier = Modifier.width(100.dp) // 제목과 동일한 너비 설정
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeDetailProgramParticipantListItemPreview() {
+    HomeDetailProgramParticipantListItem(
+        index = 1,
+        data = HomeDetailProgramParticipantData(
+            name = "이명훈",
+            company = "초등학교",
+            position = "교사",
+            schoolSubject = "컴퓨터공학",
+            phone = "010-1234-5678"
+        )
+    )
+}

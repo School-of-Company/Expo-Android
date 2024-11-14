8ce1723a3521a01be52e69c201c82265ad7fca06
명훈 <s23065@gsm.hs.kr>
2024-11-14 23:02:27 +0900

    :recycle: :: Use PaddingValues

--- app/src/main/java/com/school_of_company/expo_android/ui/ExpoApp.kt
+++ app/src/main/java/com/school_of_company/expo_android/ui/ExpoApp.kt
@@ -1,7 +1,9 @@
 package com.school_of_company.expo_android.ui
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavDestination
@@ -64,9 +67,12 @@ fun ExpoApp(
                     )
                 }
             }
-        ) { _ ->
+        ) { paddingValues ->
             // 네비게이션 호스트
-            ExpoNavHost(appState = appState)
+            Box(modifier = Modifier.padding(paddingValues =
+            paddingValues)) {
+                ExpoNavHost(appState = appState)
+            }
         }
     }
 }

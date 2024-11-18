81360897c32f6da3d318d94934111baa4cf55187
명훈 <s23065@gsm.hs.kr>
2024-11-18 17:39:07 +0900

    :recycle: :: Changed Naming(expoList -> data)

--- feature/expo/src/main/java/com/school_of_company/expo/viewmodel/uistate/GetExpoListUiState.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/viewmodel/uistate/GetExpoListUiState.kt
@@ -5,6 +5,6 @@ import com.school_of_company.model.entity.expo.ExpoListResponseEntity
 sealed interface GetExpoListUiState {
     object Loading : GetExpoListUiState
     object Empty : GetExpoListUiState
-    data class Success(val expoList: List<ExpoListResponseEntity>) : GetExpoListUiState
+    data class Success(val data: List<ExpoListResponseEntity>) : GetExpoListUiState
     data class Error(val exception: Throwable) : GetExpoListUiState
 }

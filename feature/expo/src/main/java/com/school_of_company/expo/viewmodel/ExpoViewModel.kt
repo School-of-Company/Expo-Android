4aa4b8927a0bc480ef56680c012b9f09ffd40ea3
명훈 <s23065@gsm.hs.kr>
2024-11-19 18:25:30 +0900

    :recycle: :: Refactor ExpoViewModel

--- feature/expo/src/main/java/com/school_of_company/expo/viewmodel/ExpoViewModel.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/viewmodel/ExpoViewModel.kt
@@ -10,8 +10,14 @@ import com.school_of_company.domain.usecase.expo.GetExpoInformationUseCase
 import com.school_of_company.domain.usecase.expo.GetExpoListUseCase
 import com.school_of_company.domain.usecase.expo.ModifyExpoInformationUseCase
 import com.school_of_company.domain.usecase.expo.RegisterExpoInformationUseCase
+import com.school_of_company.expo.viewmodel.uistate.DeleteExpoInformationUiState
+import com.school_of_company.expo.viewmodel.uistate.GetExpoInformationUiState
+import com.school_of_company.expo.viewmodel.uistate.GetExpoListUiState
+import com.school_of_company.expo.viewmodel.uistate.RegisterExpoInformationUiState
+import com.school_of_company.model.entity.expo.ExpoListResponseEntity
 import com.school_of_company.model.model.expo.ExpoRequestAndResponseModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
@@ -29,40 +35,32 @@ class ExpoViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     companion object {
-        private const val TITLE = "title"
-        private const val CONTENT = "content"
         private const val MODIFY_TITLE = "modify_title"
         private const val STARTED_DATE = "started_date"
         private const val ENDED_DATE = "ended_date"
         private const val INTRODUCE_TITLE = "introduce_title"
         private const val ADDRESS = "address"
         private const val LOCATION = "location"
+        private const val COVER_IMAGE = "cover_image"
     }
+    private val _swipeRefreshLoading = MutableStateFlow(false)
+    val swipeRefreshLoading = _swipeRefreshLoading.asStateFlow()
 
-    private val _getExpoInformationUiState = MutableStateFlow<com.school_of_company.expo.viewmodel.uistate.GetExpoInformationUiState>(
-        com.school_of_company.expo.viewmodel.uistate.GetExpoInformationUiState.Loading)
+    private val _getExpoInformationUiState = MutableStateFlow<GetExpoInformationUiState>(GetExpoInformationUiState.Loading)
     internal val getExpoInformationUiState = _getExpoInformationUiState.asStateFlow()
 
-    private val _registerExpoInformationUiState = MutableStateFlow<com.school_of_company.expo.viewmodel.uistate.RegisterExpoInformationUiState>(
-        com.school_of_company.expo.viewmodel.uistate.RegisterExpoInformationUiState.Loading)
+    private val _registerExpoInformationUiState = MutableStateFlow<RegisterExpoInformationUiState>(RegisterExpoInformationUiState.Loading)
     internal val registerExpoInformationUiState = _registerExpoInformationUiState.asStateFlow()
 
     private val _modifyExpoInformationUiState = MutableStateFlow<com.school_of_company.expo.viewmodel.uistate.ModifyExpoInformationUiState>(
         com.school_of_company.expo.viewmodel.uistate.ModifyExpoInformationUiState.Loading)
     internal val modifyExpoInformationUiState = _modifyExpoInformationUiState.asStateFlow()
 
-    private val _deleteExpoInformationUiState = MutableStateFlow<com.school_of_company.expo.viewmodel.uistate.DeleteExpoInformationUiState>(
-        com.school_of_company.expo.viewmodel.uistate.DeleteExpoInformationUiState.Loading)
+    private val _deleteExpoInformationUiState = MutableStateFlow<DeleteExpoInformationUiState>(DeleteExpoInformationUiState.Loading)
     internal val deleteExpoInformationUiState = _deleteExpoInformationUiState.asStateFlow()
 
-    private val _getExpoListUiState = MutableStateFlow<com.school_of_company.expo.viewmodel.uistate.GetExpoListUiState>(
-        com.school_of_company.expo.viewmodel.uistate.GetExpoListUiState.Loading)
+    private val _getExpoListUiState = MutableStateFlow<GetExpoListUiState>(GetExpoListUiState.Loading)
     internal val getExpoListUiState = _getExpoListUiState.asStateFlow()
-
-
-    internal var title = savedStateHandle.getStateFlow(key = TITLE, initialValue = "")
-
-    internal var content = savedStateHandle.getStateFlow(key = CONTENT, initialValue = "")
 
     internal var modify_title = savedStateHandle.getStateFlow(key = MODIFY_TITLE, initialValue = "")
 
@@ -76,31 +74,48 @@ class ExpoViewModel @Inject constructor(
 
     internal var location = savedStateHandle.getStateFlow(key = LOCATION, initialValue = "")
 
+    internal var cover_image = savedStateHandle.getStateFlow(key = COVER_IMAGE, initialValue = "")
+
     internal fun getExpoInformation(expoId: Long) = viewModelScope.launch {
         getExpoInformationUseCase(expoId = expoId)
             .asResult()
             .collectLatest { result ->
                 when (result) {
-                    is Result.Loading -> _getExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.GetExpoInformationUiState.Loading
-                    is Result.Success -> _getExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.GetExpoInformationUiState.Success(result.data)
-                    is Result.Error -> _getExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.GetExpoInformationUiState.Error(result.exception)
+                    is Result.Loading -> _getExpoInformationUiState.value = GetExpoInformationUiState.Loading
+                    is Result.Success -> {
+                        _getExpoInformationUiState.value = GetExpoInformationUiState.Success(result.data)
+
+                        result.data.let {
+                            onModifyTitleChange(it.title)
+                            onStartedDateChange(it.startedDay)
+                            onEndedDateChange(it.finishedDay)
+                            onIntroduceTitleChange(it.description)
+                            onLocationChange(it.location)
+                            onCoverImageChange(it.coverImage)
+                        }
+                    }
+                    is Result.Error -> _getExpoInformationUiState.value = GetExpoInformationUiState.Error(result.exception)
                 }
             }
     }
 
     internal fun registerExpoInformation(body: ExpoRequestAndResponseModel) = viewModelScope.launch {
-        _registerExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.RegisterExpoInformationUiState.Loading
+        _registerExpoInformationUiState.value = RegisterExpoInformationUiState.Loading
         registerExpoInformationUseCase(body = body)
             .onSuccess {
                 it.catch { remoteError ->
-                    _registerExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.RegisterExpoInformationUiState.Error(remoteError)
+                    _registerExpoInformationUiState.value = RegisterExpoInformationUiState.Error(remoteError)
                 }.collect {
-                    _registerExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.RegisterExpoInformationUiState.Success
+                    _registerExpoInformationUiState.value = RegisterExpoInformationUiState.Success
                 }
             }
             .onFailure { error ->
-                _registerExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.RegisterExpoInformationUiState.Error(error)
+                _registerExpoInformationUiState.value = RegisterExpoInformationUiState.Error(error)
             }
+    }
+
+    internal fun initRegisterExpo() {
+        _registerExpoInformationUiState.value = RegisterExpoInformationUiState.Loading
     }
 
     internal fun modifyExpoInformation(
@@ -125,46 +140,54 @@ class ExpoViewModel @Inject constructor(
             }
     }
 
+    internal fun resetExpoInformation() {
+        onStartedDateChange("")
+        onEndedDateChange("")
+        onIntroduceTitleChange("")
+        onAddressChange("")
+        onLocationChange("")
+        onCoverImageChange(null)
+        onModifyTitleChange("")
+    }
+
     internal fun deleteExpoInformation(expoId: Long) = viewModelScope.launch {
-        _deleteExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.DeleteExpoInformationUiState.Loading
+        _deleteExpoInformationUiState.value = DeleteExpoInformationUiState.Loading
         deleteExpoInformationUseCase(expoId = expoId)
             .onSuccess {
                 it.catch { remoteError ->
-                    _deleteExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.DeleteExpoInformationUiState.Error(remoteError)
+                    _deleteExpoInformationUiState.value = DeleteExpoInformationUiState.Error(remoteError)
                 }.collect {
-                    _deleteExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.DeleteExpoInformationUiState.Success
+                    _deleteExpoInformationUiState.value = DeleteExpoInformationUiState.Success
 
                 }
             }
             .onFailure { error ->
-                _deleteExpoInformationUiState.value = com.school_of_company.expo.viewmodel.uistate.DeleteExpoInformationUiState.Error(error)
+                _deleteExpoInformationUiState.value = DeleteExpoInformationUiState.Error(error)
             }
     }
 
     internal fun expoList() = viewModelScope.launch {
+        _swipeRefreshLoading.value = true
         getExpoListUseCase()
             .asResult()
             .collectLatest { result ->
                 when (result) {
-                    is Result.Loading -> _getExpoListUiState.value = com.school_of_company.expo.viewmodel.uistate.GetExpoListUiState.Loading
+                    is Result.Loading -> _getExpoListUiState.value = GetExpoListUiState.Loading
                     is Result.Success -> {
                         if (result.data.isEmpty()) {
-                            _getExpoListUiState.value = com.school_of_company.expo.viewmodel.uistate.GetExpoListUiState.Empty
+                            _getExpoListUiState.value = GetExpoListUiState.Empty
+                            _swipeRefreshLoading.value = false
                         } else {
-                            _getExpoListUiState.value = com.school_of_company.expo.viewmodel.uistate.GetExpoListUiState.Success(result.data)
+                            _getExpoListUiState.value = GetExpoListUiState.Success(result.data)
+                            _swipeRefreshLoading.value = false
                         }
                     }
-                    is Result.Error -> _getExpoListUiState.value = com.school_of_company.expo.viewmodel.uistate.GetExpoListUiState.Error(result.exception)
+                    is Result.Error -> {
+                        _getExpoListUiState.value = GetExpoListUiState.Error(result.exception)
+                        _swipeRefreshLoading.value = false
+                    }
                 }
             }
-    }
-
-    internal fun onTitleChange(value: String) {
-        savedStateHandle[TITLE] = value
-    }
-
-    internal fun onContentChange(value: String) {
-        savedStateHandle[CONTENT] = value
     }
 
     internal fun onModifyTitleChange(value: String) {
@@ -189,5 +212,9 @@ class ExpoViewModel @Inject constructor(
 
     internal fun onLocationChange(value: String) {
         savedStateHandle[LOCATION] = value
+    }
+
+    internal fun onCoverImageChange(value: String?) {
+        savedStateHandle[COVER_IMAGE] = value
     }
 }

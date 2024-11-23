94f8c4acdb2ffa3644941dbb28cd3c5eb9d47df7
명훈 <s23065@gsm.hs.kr>
2024-11-23 11:37:48 +0900

    :fire: :: Fire Unused Import

--- feature/expo/src/main/java/com/school_of_company/expo/view/ExpoScreen.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/view/ExpoScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -126,6 +125,8 @@ internal fun ExpoScreen(
                     text = filterButtonText
                 ) { isOpenBottomSheet(true) }
             }
+
+            Spacer(modifier = Modifier.padding(bottom = 24.dp))
 
             SwipeRefresh(
                 state = swipeRefreshState,

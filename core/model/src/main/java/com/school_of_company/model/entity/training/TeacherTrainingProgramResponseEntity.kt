96d0e38ede67951132af633e2340902f0396a58e
명훈 <s23065@gsm.hs.kr>
2024-11-25 23:12:42 +0900

    :sparkles: :: Create Training Model & Entity

---
+++ core/model/src/main/java/com/school_of_company/model/entity/training/TeacherTrainingProgramResponseEntity.kt
@@ -0,0 +1,11 @@
+package com.school_of_company.model.entity.training
+
+data class TeacherTrainingProgramResponseEntity(
+    val name: String,
+    val organization: String,
+    val position: String,
+    val programName: String,
+    val status: Boolean,
+    val entryTime: String,
+    val leaveTime: String
+)
---
+++ core/model/src/main/java/com/school_of_company/model/model/training/TrainingDtoModel.kt
@@ -0,0 +1,8 @@
+package com.school_of_company.model.model.training
+
+data class TrainingDtoModel(
+    val title: String,
+    val startedAt: String,
+    val finishedAt: String,
+    val category: String,
+)

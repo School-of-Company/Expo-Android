c31a9e547feccbf8d2182f7cdc07b06819fe4469
명훈 <s23065@gsm.hs.kr>
2024-11-30 23:19:14 +0900

    :recycle: :: Apply API Changed

--- core/model/src/main/java/com/school_of_company/model/entity/training/TrainingProgramListResponseEntity.kt
+++ core/model/src/main/java/com/school_of_company/model/entity/training/TrainingProgramListResponseEntity.kt
@@ -1,12 +1,9 @@
 package com.school_of_company.model.entity.training
 
 data class TrainingProgramListResponseEntity(
-    val essential: List<Training>,
-    val choice: List<Training>
-) {
-    data class Training(
-        val title: String,
-        val startedAt: String,
-        val endedAt: String,
-    )
-}
+    val id: Long,
+    val title: String,
+    val startedAt: String,
+    val endedAt: String,
+    val category: String
+)

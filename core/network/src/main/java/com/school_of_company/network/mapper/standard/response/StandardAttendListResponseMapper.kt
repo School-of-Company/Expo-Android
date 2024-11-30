2d69ed4701b37617fcbdeac9793f5f54c9df0e46
명훈 <s23065@gsm.hs.kr>
2024-12-01 00:42:28 +0900

    :sparkles: :: Create Standard Mapper

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/standard/response/StandardAttendListResponseMapper.kt
@@ -0,0 +1,15 @@
+package com.school_of_company.network.mapper.standard.response
+
+import com.school_of_company.model.entity.standard.StandardAttendListResponseEntity
+import com.school_of_company.network.dto.standard.response.StandardAttendListResponse
+
+fun StandardAttendListResponse.toModel(): StandardAttendListResponseEntity =
+    StandardAttendListResponseEntity(
+        name = this.name,
+        affiliation = this.affiliation,
+        position = this.position,
+        programName = this.programName,
+        status = this.status,
+        entryTime = this.entryTime,
+        leaveTime = this.leaveTime
+    )

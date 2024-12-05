563d502ba17f13fb5e9755ca966fd2857a7efc82
명훈 <s23065@gsm.hs.kr>
2024-12-05 14:23:39 +0900

    :memo: :: Change Server(Nullable)

--- core/model/src/main/java/com/school_of_company/model/entity/training/TeacherTrainingProgramResponseEntity.kt
+++ core/model/src/main/java/com/school_of_company/model/entity/training/TeacherTrainingProgramResponseEntity.kt
@@ -1,11 +1,12 @@
 package com.school_of_company.model.entity.training
 
 data class TeacherTrainingProgramResponseEntity(
+    val id: Long,
     val name: String,
     val organization: String,
     val position: String,
     val programName: String,
     val status: Boolean,
-    val entryTime: String,
-    val leaveTime: String
+    val entryTime: String?,
+    val leaveTime: String?
 )

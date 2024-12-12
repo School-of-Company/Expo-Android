67e6f277c40eea55d04013a6b06e0757a2563886
명훈 <s23065@gsm.hs.kr>
2024-12-12 15:13:32 +0900

    :lipstick: :: Add HomeDetailStandardParticipantList

--- feature/home/src/main/java/com/school_of_company/home/view/component/HomeDetailProgramParticipantList.kt
+++ feature/home/src/main/java/com/school_of_company/home/view/component/HomeDetailProgramParticipantList.kt
@@ -6,11 +6,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.school_of_company.design_system.theme.ExpoAndroidTheme
+import com.school_of_company.model.entity.standard.StandardAttendListResponseEntity
 import com.school_of_company.model.entity.training.TeacherTrainingProgramResponseEntity
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
@@ -29,6 +29,28 @@ fun HomeDetailProgramParticipantList(
         ) {
             itemsIndexed(item) { index, item ->
                 HomeDetailProgramParticipantListItem(
+                    index = index + 1,
+                    data = item
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeDetailStandardParticipantList(
+    modifier: Modifier = Modifier,
+    item: ImmutableList<StandardAttendListResponseEntity> = persistentListOf()
+) {
+    ExpoAndroidTheme { colors, _ ->
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color = colors.white)
+                .padding(start = 16.dp)
+        ) {
+            itemsIndexed(item) { index, item ->
+                HomeDetailStandardProgramParticipantListItem(
                     index = index + 1,
                     data = item
                 )

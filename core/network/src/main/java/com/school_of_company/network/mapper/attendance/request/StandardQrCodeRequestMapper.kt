25e5a69530c69298f55afc24c1ffc1dfbeb68ae8
명훈 <s23065@gsm.hs.kr>
2025-01-02 10:11:15 +0900

    :sparkles: :: Create StandardQrCodeRequestMapper

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/attendance/request/StandardQrCodeRequestMapper.kt
@@ -0,0 +1,10 @@
+package com.school_of_company.network.mapper.attendance.request
+
+import com.school_of_company.model.param.attendance.StandardQrCodeRequestParam
+import com.school_of_company.network.dto.attendance.request.StandardQrCodeRequest
+
+fun StandardQrCodeRequestParam.toDto(): StandardQrCodeRequest =
+    StandardQrCodeRequest(
+        participantId = this.participantId,
+        phoneNumber = this.phoneNumber
+    )

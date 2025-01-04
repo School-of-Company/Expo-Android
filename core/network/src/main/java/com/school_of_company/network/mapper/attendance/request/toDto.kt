337c673015ac8975e85296291273f2edc2f7d1e3
youloveli <mymhj122@gmail.com>
2025-01-04 20:25:05 +0900

    :sparkles: :: StandardQrCodeRequestParam mapper(toDto) 삭제

--- core/network/src/main/java/com/school_of_company/network/mapper/attendance/request/toDto.kt
+++
@@ -1,7 +0,0 @@
-package com.school_of_company.network.mapper.attendance.request
-
-import com.school_of_company.model.param.attendance.StandardQrCodeRequestParam
-import com.school_of_company.network.dto.attendance.request.StandardQrCodeRequest
-
-fun StandardQrCodeRequestParam.toDto(): StandardQrCodeRequest =
-    StandardQrCodeRequest(participantId = participantId, phoneNumber = phoneNumber)

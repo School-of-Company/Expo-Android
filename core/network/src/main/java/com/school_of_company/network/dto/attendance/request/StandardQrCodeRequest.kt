4bcc3991fbbe302ed036337319074835e92861bd
youloveli <mymhj122@gmail.com>
2025-01-07 10:59:05 +0900

    :recycle: :: 코드 컨벤션에 맞게 코드 변경

--- core/network/src/main/java/com/school_of_company/network/dto/attendance/request/StandardQrCodeRequest.kt
+++ core/network/src/main/java/com/school_of_company/network/dto/attendance/request/StandardQrCodeRequest.kt
@@ -1,9 +1,10 @@
 package com.school_of_company.network.dto.attendance.request
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class StandardQrCodeRequest(
-    val participantId: Long,
-    val phoneNumber: String,
+    @Json(name = "participantId") val participantId: Long,
+    @Json(name = "phoneNumber") val phoneNumber: String,
 )

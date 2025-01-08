cee77b82f10676bf81e37c7b95d1780c42bf67a4
youloveli <mymhj122@gmail.com>
2025-01-08 20:49:51 +0900

    :sparkles: :: CreatedExpoListItem 퍼블리싱

---
+++ feature/expo/src/main/java/com/school_of_company/expo/view/component/CreatedExpoListItem.kt
@@ -0,0 +1,108 @@
+package com.school_of_company.expo.view.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.school_of_company.design_system.icon.CircleIcon
+import com.school_of_company.design_system.icon.XIcon
+import com.school_of_company.design_system.theme.ExpoAndroidTheme
+import com.school_of_company.design_system.theme.ExpoTypography
+import com.school_of_company.design_system.theme.color.ColorTheme
+
+@Composable
+fun CreatedExpoListItem(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean,
+    id: Long,
+    title: String,
+    startedDay: String,
+    finishedDay: String,
+    coverImage: String?
+) {
+    ExpoAndroidTheme { colors: ColorTheme, typography: ExpoTypography ->
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(38.dp, Alignment.Start),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .background(
+                    color = if (isSelected) colors.main100 else colors.white,
+                    shape = RoundedCornerShape(size = 4.dp)
+                )
+                .padding(8.dp),
+        ) {
+            Text(
+                text = id.toString(),
+                style = typography.captionBold1,
+                fontWeight = FontWeight.W600,
+                color = colors.black,
+                textAlign = TextAlign.Center,
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(48.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                if (coverImage == null) {
+                    XIcon()
+                } else {
+                    CircleIcon()
+                }
+                Text(
+                    text = title,
+                    style = typography.captionRegular2,
+                    fontWeight = FontWeight.W400,
+                    color = colors.black,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = startedDay,
+                    style = typography.captionRegular2,
+                    fontWeight = FontWeight.W400,
+                    color = colors.black,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = finishedDay,
+                    style = typography.captionRegular2,
+                    fontWeight = FontWeight.W400,
+                    color = colors.black,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CreatedExpoListItemNotSelectedPreview() {
+    CreatedExpoListItem(
+        id = 2,
+        coverImage = null,
+        startedDay = "시작날",
+        finishedDay = "끝날",
+        title = "제목입니다",
+        isSelected = false,
+    )
+}
+@Preview
+@Composable
+fun CreatedExpoListItemSelectedPreview() {
+    CreatedExpoListItem(
+        id = 2,
+        coverImage = null,
+        startedDay = "시작날",
+        finishedDay = "끝날",
+        title = "제목입니다",
+        isSelected = true,
+    )
+}

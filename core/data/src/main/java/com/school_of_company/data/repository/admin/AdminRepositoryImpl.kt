b82db3b247be7fcf2cdaa5d400eb9661db2fa4aa
명훈 <s23065@gsm.hs.kr>
2025-01-09 14:28:42 +0900

    :sparkles: :: Create AdminRepository(Impl)

---
+++ core/data/src/main/java/com/school_of_company/data/repository/admin/AdminRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.school_of_company.data.repository.admin
+
+import com.school_of_company.model.entity.admin.AdminRequestAllowListResponseEntity
+import com.school_of_company.network.datasource.admin.AdminDataSource
+import com.school_of_company.network.mapper.admin.response.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
+import javax.inject.Inject
+
+class AdminRepositoryImpl @Inject constructor(
+    private val dataSource: AdminDataSource
+) : AdminRepository {
+    override fun getAdminRequestAllowList(): Flow<List<AdminRequestAllowListResponseEntity>> {
+        return dataSource.getAdminRequestAllowList().transform { list ->
+            emit(list.map { it.toEntity() })
+        }
+    }
+
+    override fun allowAdmin(adminId: Long): Flow<Unit> {
+        return dataSource.allowAdmin(adminId = adminId)
+    }
+
+    override fun serviceWithdrawal(): Flow<Unit> {
+        return dataSource.serviceWithdrawal()
+    }
+}

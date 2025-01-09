aea1c42f932cbb2f3a5b3e332069f2f9eda5d573
명훈 <s23065@gsm.hs.kr>
2025-01-09 14:39:46 +0900

    :sparkles: :: Create GetAdminRequestAllowListUseCase

---
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/admin/GetAdminRequestAllowListUseCase.kt
@@ -0,0 +1,13 @@
+package com.school_of_company.domain.usecase.admin
+
+import com.school_of_company.data.repository.admin.AdminRepository
+import com.school_of_company.model.entity.admin.AdminRequestAllowListResponseEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetAdminRequestAllowListUseCase @Inject constructor(
+    private val repository: AdminRepository
+) {
+    operator fun invoke(): Flow<List<AdminRequestAllowListResponseEntity>> =
+        repository.getAdminRequestAllowList()
+}

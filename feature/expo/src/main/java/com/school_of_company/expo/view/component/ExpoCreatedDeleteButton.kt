47b9410b7664f8207463d5b0b836488f2c2710d5
youloveli <mymhj122@gmail.com>
2025-01-09 15:19:45 +0900

    :sparkles: :: ExpoCreatedDeleteButton 퍼블리싱

---
+++ feature/expo/src/main/java/com/school_of_company/expo/view/component/ExpoCreatedDeleteButton.kt
@@ -0,0 +1,64 @@
+package com.school_of_company.expo.view.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.school_of_company.design_system.icon.TrashIcon
+import com.school_of_company.design_system.theme.ExpoAndroidTheme
+import com.school_of_company.design_system.theme.ExpoTypography
+import com.school_of_company.design_system.theme.color.ColorTheme
+
+@Composable
+fun ExpoCreatedDeleteButton(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    ExpoAndroidTheme { colors: ColorTheme, typography: ExpoTypography ->
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .background(
+                    color = if (enabled) colors.error
+                    else colors.white,
+                    shape = RoundedCornerShape(size = 6.dp)
+                )
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+        ) {
+            TrashIcon(
+                tint = if (enabled) colors.white
+                else colors.gray400,
+            )
+            Text(
+                text = "삭제하기",
+                style = typography.titleBold3,
+                fontWeight = FontWeight.W600,
+                color = if (enabled) colors.white
+                else colors.gray400,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExpoCreatedDeleteButtonEnabled() {
+    ExpoCreatedDeleteButton(enabled = true)
+}
+
+@Preview
+@Composable
+fun ExpoCreatedDeleteButtonDisabled() {
+    ExpoCreatedDeleteButton(enabled = false)
+}

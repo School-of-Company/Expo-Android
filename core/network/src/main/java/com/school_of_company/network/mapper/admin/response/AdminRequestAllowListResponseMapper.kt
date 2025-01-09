092a72ce7934d9a27351ca996e2f1d2629d4bac3
명훈 <s23065@gsm.hs.kr>
2025-01-09 14:45:49 +0900

    :memo: :: Change Code

--- core/network/src/main/java/com/school_of_company/network/mapper/admin/response/AdminRequestAllowListResponseMapper.kt
+++ core/network/src/main/java/com/school_of_company/network/mapper/admin/response/AdminRequestAllowListResponseMapper.kt
@@ -3,8 +3,8 @@ package com.school_of_company.network.mapper.admin.response
 import com.school_of_company.model.entity.admin.AdminRequestAllowListResponseEntity
 import com.school_of_company.network.dto.admin.response.AdminRequestAllowListResponse
 
-fun AdminRequestAllowListResponseEntity.toEntity(): AdminRequestAllowListResponse =
-    AdminRequestAllowListResponse(
+fun AdminRequestAllowListResponse.toEntity(): AdminRequestAllowListResponseEntity =
+    AdminRequestAllowListResponseEntity(
         id = this.id,
         name = this.name,
         nickname = this.nickname,

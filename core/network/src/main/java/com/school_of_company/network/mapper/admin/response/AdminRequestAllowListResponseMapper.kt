0d4c689f140ffb560c668dadee74ff02d8521f8f
명훈 <s23065@gsm.hs.kr>
2025-01-09 14:14:47 +0900

    :sparkles: :: Create AdminRequestAllowListResponseMapper

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/admin/response/AdminRequestAllowListResponseMapper.kt
@@ -0,0 +1,13 @@
+package com.school_of_company.network.mapper.admin.response
+
+import com.school_of_company.model.entity.admin.AdminRequestAllowListResponseEntity
+import com.school_of_company.network.dto.admin.response.AdminRequestAllowListResponse
+
+fun AdminRequestAllowListResponseEntity.toEntity(): AdminRequestAllowListResponse =
+    AdminRequestAllowListResponse(
+        id = this.id,
+        name = this.name,
+        nickname = this.nickname,
+        email = this.email,
+        phoneNumber = this.phoneNumber
+    )

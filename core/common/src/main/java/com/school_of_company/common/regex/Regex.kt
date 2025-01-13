5f2f36405aa22a8a97b709dd043f7516c096f4f5
youloveli <mymhj122@gmail.com>
2025-01-13 20:56:04 +0900

    :sparkles: :: isValidDate, isValidDateTime 함수 추가

--- core/common/src/main/java/com/school_of_company/common/regex/Regex.kt
+++ core/common/src/main/java/com/school_of_company/common/regex/Regex.kt
@@ -1,7 +1,22 @@
 package com.school_of_company.common.regex
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 fun String.checkEmailRegex() =
     this.matches(".*@.*".toRegex())
 
 fun String.checkPasswordRegex() =
     this.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d\$!%*?&]{8,24}$".toRegex())
+
+
+fun String.isValidDate(): Boolean =
+    this.matches(Regex("\\d{4}-\\d{2}-\\d{2}")) && runCatching {
+        LocalDate.parse(this, DateTimeFormatter.ISO_LOCAL_DATE)
+    }.isSuccess
+
+fun String.isValidDateTime(): Boolean =
+    this.matches(Regex("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}")) && runCatching {
+        LocalDateTime.parse(this, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+    }.isSuccess

a6b51921f8082fc0768ba86db90aada30428fa77
youloveli <mymhj122@gmail.com>
2025-01-15 23:01:18 +0900

    :sparkles: :: sendSmsToParticipantTrainee  api 추가

--- core/network/src/main/java/com/school_of_company/network/api/SmsAPI.kt
+++ core/network/src/main/java/com/school_of_company/network/api/SmsAPI.kt
@@ -1,9 +1,11 @@
 package com.school_of_company.network.api
 
 import com.school_of_company.network.dto.sms.request.SmsSignUpCertificationNumberSendRequest
+import com.school_of_company.network.dto.sms.request.SendSmsToParticipantTraineeResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SmsAPI {
@@ -11,6 +13,12 @@ interface SmsAPI {
     @POST("/sms")
     suspend fun smsSignUpCertificationNumberSend(
         @Body body: SmsSignUpCertificationNumberSendRequest
+    )
+
+    @POST("/sms/message/{expo_id}")
+    suspend fun sendSmsToParticipantTrainee(
+        @Path("expo_id") id: String,
+        @Body body: SendSmsToParticipantTraineeResponse,
     )
 
     @GET("/sms")

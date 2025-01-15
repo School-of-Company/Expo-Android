d7c5d23ff72de270738ef5a403a38ec4ad2f8c2b
answad <mymhj122@gmail.com>
2025-01-15 20:39:57 +0900

    :sparkles: :: navigateToSmsSendMessage이 smsType을 받을수 있도록 수정

--- feature/sms/src/main/java/com/school_of_company/sms/navigation/SmsNavigation.kt
+++ feature/sms/src/main/java/com/school_of_company/sms/navigation/SmsNavigation.kt
@@ -8,16 +8,27 @@ import com.school_of_company.sms.view.SendMessageRoute
 
 const val smsSendMessageRoute = "sms_send_message_route"
 
-fun NavController.navigateToSmsSendMessage(navOptions: NavOptions? = null) {
-    this.navigate(smsSendMessageRoute, navOptions)
+fun NavController.navigateToSmsSendMessage(
+    smsType: String,
+    navOptions: NavOptions? = null,
+) {
+    this.navigate(
+        "$smsSendMessageRoute/${smsType}",
+        navOptions,
+    )
 }
 
 fun NavGraphBuilder.smsSendMessageScreen(
     onBackClick: () -> Unit
 ) {
-    composable(route = smsSendMessageRoute) {
-        SendMessageRoute(
-            onBackClick = onBackClick
-        )
+    composable(route = "$smsSendMessageRoute/{smsType}") { backStackEntry ->
+        val smsType = backStackEntry.arguments?.getString("smsType")
+
+        if (smsType != null) {
+            SendMessageRoute(
+                onBackClick = onBackClick,
+                smsType = smsType,
+            )
+        }
     }
 }

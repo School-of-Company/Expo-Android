1041735ed3ba704cf3c389b1cd34c419245bf984
youloveli <mymhj122@gmail.com>
2025-01-15 23:01:39 +0900

    :sparkles: :: sendSmsToParticipantTrainee  dataSource 추가

--- core/network/src/main/java/com/school_of_company/network/datasource/sms/SmsDataSourceImpl.kt
+++ core/network/src/main/java/com/school_of_company/network/datasource/sms/SmsDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.school_of_company.network.datasource.sms
 
 import com.school_of_company.network.api.SmsAPI
 import com.school_of_company.network.dto.sms.request.SmsSignUpCertificationNumberSendRequest
+import com.school_of_company.network.dto.sms.request.SendSmsToParticipantTraineeResponse
 import com.school_of_company.network.util.performApiRequest
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -12,9 +13,25 @@ class SmsDataSourceImpl @Inject constructor(
     override fun smsSignUpCertificationNumberSend(body: SmsSignUpCertificationNumberSendRequest): Flow<Unit> =
         performApiRequest { service.smsSignUpCertificationNumberSend(body = body) }
 
-    override fun smsSignUpCertificationNumberCertification(phoneNumber: String, code: String): Flow<Unit> =
-        performApiRequest { service.smsSignUpCertificationNumberCertification(
-            phoneNumber = phoneNumber,
-            code = code
-        ) }
+    override fun sendSmsToParticipantTrainee(
+        id: String,
+        body: SendSmsToParticipantTraineeResponse
+    ): Flow<Unit> =
+        performApiRequest {
+            service.sendSmsToParticipantTrainee(
+                id = id,
+                body = body,
+            )
+        }
+
+    override fun smsSignUpCertificationNumberCertification(
+        phoneNumber: String,
+        code: String
+    ): Flow<Unit> =
+        performApiRequest {
+            service.smsSignUpCertificationNumberCertification(
+                phoneNumber = phoneNumber,
+                code = code
+            )
+        }
 }

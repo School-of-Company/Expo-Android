a31f4b79df889dce143168f6dae4dcf2e3bf8ac5
answad <mymhj122@gmail.com>
2025-01-17 16:12:47 +0900

    :sparkles: :: SendSmsUiState 추가후 sendSmsToParticipantTrainee 함수에 _sendSmsUiState 연결

--- feature/sms/src/main/java/com/school_of_company/sms/viewmodel/SmsViewModel.kt
+++ feature/sms/src/main/java/com/school_of_company/sms/viewmodel/SmsViewModel.kt
@@ -5,7 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.school_of_company.domain.usecase.sms.SendSmsToParticipantTraineeUseCase
 import com.school_of_company.model.param.sms.SendSmsToParticipantTraineeParam
+import com.school_of_company.sms.viewmodel.uiState.SendSmsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,6 +22,8 @@ internal class SmsViewModel @Inject constructor(
         private const val TITLE = "title"
         private const val CONTENT = "content"
     }
+    private val _sendSmsUiState = MutableStateFlow<SendSmsUiState>(SendSmsUiState.Loading)
+    internal val sendSmsUiState = _sendSmsUiState.asStateFlow()
 
     internal var title = savedStateHandle.getStateFlow(key = TITLE, initialValue = "")
 
@@ -28,6 +34,16 @@ internal class SmsViewModel @Inject constructor(
         body: SendSmsToParticipantTraineeParam
     ) = viewModelScope.launch {
         sendSmsToParticipantTraineeUseCase(id, body)
+            .onSuccess {
+                it.catch { remoteError ->
+                    _sendSmsUiState.value = SendSmsUiState.Error(remoteError)
+                }.collect {
+                    _sendSmsUiState.value = SendSmsUiState.Success
+                }
+            }
+            .onFailure { error ->
+                _sendSmsUiState.value = SendSmsUiState.Error(error)
+            }
     }
 
     internal fun onTitleChange(value: String) {

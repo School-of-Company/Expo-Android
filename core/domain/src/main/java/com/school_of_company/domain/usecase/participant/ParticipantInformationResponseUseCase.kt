a0ff05c1cee19dfea84c9f5fc03efa69553f0e74
명훈 <s23065@gsm.hs.kr>
2025-01-17 19:36:44 +0900

    :sparkles: :: Create ParticipantInformationResponseUseCase

---
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/participant/ParticipantInformationResponseUseCase.kt
@@ -0,0 +1,19 @@
+package com.school_of_company.domain.usecase.participant
+
+import com.school_of_company.data.repository.participant.ParticipantRepository
+import com.school_of_company.model.entity.participant.ParticipantInformationResponseEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ParticipantInformationResponseUseCase @Inject constructor(
+    private val repository: ParticipantRepository
+) {
+    operator fun invoke(
+        type: String,
+        expoId: String
+    ): Flow<List<ParticipantInformationResponseEntity>> =
+        repository.getParticipantInformationList(
+            type = type,
+            expoId = expoId
+        )
+}

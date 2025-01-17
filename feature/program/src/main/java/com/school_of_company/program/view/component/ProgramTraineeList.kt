f7c8e0f55f2ab1f08e2e8d6ac765ab82b8fc085d
명훈 <s23065@gsm.hs.kr>
2025-01-18 02:27:05 +0900

    :lipstick: :: Publishing ProgramTraineeList

---
+++ feature/program/src/main/java/com/school_of_company/program/view/component/ProgramTraineeList.kt
@@ -0,0 +1,40 @@
+package com.school_of_company.program.view.component
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.school_of_company.design_system.theme.ExpoAndroidTheme
+import com.school_of_company.model.entity.trainee.TraineeResponseEntity
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+@Composable
+internal fun ProgramTraineeList(
+    modifier: Modifier = Modifier,
+    item: ImmutableList<TraineeResponseEntity> = persistentListOf(),
+    scrollState: ScrollState
+) {
+    ExpoAndroidTheme { colors, _ ->
+
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color = colors.white)
+                .padding(start = 16.dp)
+        ) {
+            itemsIndexed(item) { index, item ->
+                ProgramTraineeListItem(
+                    index = index,
+                    data = item,
+                    horizontalScrollState = scrollState
+                )
+            }
+        }
+    }
+}

d65cb2dadb73474bd1377122408fa6b770791607
명훈 <s23065@gsm.hs.kr>
2025-01-17 19:12:22 +0900

    :recycle: :: Add Path

--- core/network/src/main/java/com/school_of_company/network/api/ParticipantAPI.kt
+++ core/network/src/main/java/com/school_of_company/network/api/ParticipantAPI.kt
@@ -2,12 +2,14 @@ package com.school_of_company.network.api
 
 import com.school_of_company.network.dto.participant.response.ParticipantInformationResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ParticipantAPI {
 
     @GET("/participant")
     suspend fun getParticipantInformationList(
-        @Query("type") type: String
+        @Query("type") type: String,
+        @Path("expo_id") expoId: String
     ) : List<ParticipantInformationResponse>
 }

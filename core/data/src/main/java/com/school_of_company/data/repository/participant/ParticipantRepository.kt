24d1cba09cc60eb6620bd103f4883fd00eb2e398
명훈 <s23065@gsm.hs.kr>
2025-01-17 19:26:04 +0900

    :sparkles: :: Create ParticipantRepository(Impl)

---
+++ core/data/src/main/java/com/school_of_company/data/repository/participant/ParticipantRepository.kt
@@ -0,0 +1,8 @@
+package com.school_of_company.data.repository.participant
+
+import com.school_of_company.model.entity.participant.ParticipantInformationResponseEntity
+import kotlinx.coroutines.flow.Flow
+
+interface ParticipantRepository {
+    fun getParticipantInformationList(type: String, expoId: String): Flow<List<ParticipantInformationResponseEntity>>
+}

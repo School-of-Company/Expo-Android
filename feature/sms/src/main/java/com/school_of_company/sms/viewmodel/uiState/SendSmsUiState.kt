9ace02cebf7a48aa5cbd45bbcd0a47fc56133295
answad <mymhj122@gmail.com>
2025-01-19 19:12:13 +0900

    :recycle: :: SendSmsUiState 를 sealed class 에서 sealed interface 로 변경

--- feature/sms/src/main/java/com/school_of_company/sms/viewmodel/uiState/SendSmsUiState.kt
+++ feature/sms/src/main/java/com/school_of_company/sms/viewmodel/uiState/SendSmsUiState.kt
@@ -1,7 +1,7 @@
 package com.school_of_company.sms.viewmodel.uiState
 
-sealed class SendSmsUiState {
-    object Loading : SendSmsUiState()
-    object Success : SendSmsUiState()
-    data class Error(val exception: Throwable) : SendSmsUiState()
+sealed interface SendSmsUiState {
+    object Loading : SendSmsUiState
+    object Success : SendSmsUiState
+    data class Error(val exception: Throwable) : SendSmsUiState
 }

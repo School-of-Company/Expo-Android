4d85bd73164b76f42f916c08fca025aaa50d1665
youloveli <mymhj122@gmail.com>
2025-01-21 23:17:42 +0900

    :recycle: :: 스플래시 호면이 로그인 요청을 보내는동안 유지되도록 수정

--- app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
+++ app/src/main/java/com/school_of_company/expo_android/MainActivity.kt
@@ -4,25 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.school_of_company.design_system.theme.ExpoAndroidTheme
-import com.school_of_company.expo.navigation.homeRoute // TODO: 둘이 다른이유 찾기  
+import com.school_of_company.expo.navigation.homeRoute
 import com.school_of_company.expo_android.ui.ExpoApp
+import com.school_of_company.navigation.sigInRoute
 import dagger.hilt.android.AndroidEntryPoint
-import com.school_of_company.navigation.sigInRoute // TODO: 둘이 다른이유 찾기 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: AppViewModel by viewModels()
 
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 스플래시 스크린을 표기합니다. 앱 시작 시 기본 스플래스 화면을 설정.
-        installSplashScreen()
+        installSplashScreen().setKeepOnScreenCondition {
+            viewModel.uiState.value is AppLoginState.Loading
+        }
 
         enableEdgeToEdge()
 

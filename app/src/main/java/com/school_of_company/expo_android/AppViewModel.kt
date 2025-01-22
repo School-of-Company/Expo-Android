7a2ee2a28239ffb248ba20e48223707302fc29f0
answad <mymhj122@gmail.com>
2025-01-22 15:01:10 +0900

    :sparkles: :: 리프래시함수 추가

--- app/src/main/java/com/school_of_company/expo_android/AppViewModel.kt
+++ app/src/main/java/com/school_of_company/expo_android/AppViewModel.kt
@@ -3,14 +3,61 @@ package com.school_of_company.expo_android
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.school_of_company.datastore.datasource.AuthTokenDataSource
+import com.school_of_company.domain.usecase.auth.AdminTokenRefreshUseCase
+import com.school_of_company.domain.usecase.auth.SaveTokenUseCase
+import com.school_of_company.model.model.auth.AdminTokenResponseModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AppViewModel @Inject constructor(
+    private val authTokenDataSource: AuthTokenDataSource,
+    private val saveTokenUseCase: SaveTokenUseCase,
+    private val adminTokenRefreshUseCase: AdminTokenRefreshUseCase,
 ) : ViewModel() {
     private val _appLoginState = mutableStateOf<AppLoginState>(AppLoginState.Loading)
-    val appLoginState: State<AppLoginState> = this._appLoginState
+    val appLoginState: State<AppLoginState> = _appLoginState
+
+    init {
+        tokenRefresh()
+    }
+
+    private fun tokenRefresh() = viewModelScope.launch {
+        _appLoginState.value = AppLoginState.Loading
+        val refreshToken = authTokenDataSource.getRefreshToken().first()
+
+        if (refreshToken.isEmpty()) {
+            _appLoginState.value = AppLoginState.Fail
+        } else {
+            adminTokenRefreshUseCase()
+                .onSuccess {
+                    it.catch {
+                        _appLoginState.value = AppLoginState.Fail
+                    }.collect { result ->
+                        saveToken(result)
+                    }
+                }
+                .onFailure {
+                    _appLoginState.value = AppLoginState.Fail
+                }
+
+        }
+    }
+
+    private suspend fun saveToken(token: AdminTokenResponseModel) =
+        saveTokenUseCase(token)
+            .onSuccess {
+                _appLoginState.value = AppLoginState.Success
+            }
+            .onFailure {
+                _appLoginState.value = AppLoginState.Fail
+            }
+
 }
 
 sealed interface AppLoginState {

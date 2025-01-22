d2453fa8e679d4087a6f82131fe617dd7fcdf0f3
answad <mymhj122@gmail.com>
2025-01-22 13:01:25 +0900

    :recycle: :: AdminTokenRefreshUseCase 반환값 변경

--- core/domain/src/main/java/com/school_of_company/domain/usecase/auth/AdminTokenRefreshUseCase.kt
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/auth/AdminTokenRefreshUseCase.kt
@@ -1,14 +1,18 @@
 package com.school_of_company.domain.usecase.auth
 
 import com.school_of_company.data.repository.auth.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class AdminTokenRefreshUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
-    suspend operator fun invoke() = runCatching {
-        authRepository.adminTokenRefresh().collect { token ->
-            authRepository.saveToken(token)
-        }
+    operator fun invoke(): Flow<Unit> = flow {
+        emit(
+            authRepository.adminTokenRefresh().collect { token ->
+                authRepository.saveToken(token)
+            }
+        )
     }
 }

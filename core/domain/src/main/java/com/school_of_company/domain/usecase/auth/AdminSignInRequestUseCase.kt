a755205a0b6a0437e23b711062896eb854a2d9dd
answad <mymhj122@gmail.com>
2025-01-22 12:25:00 +0900

    :recycle: :: signInUseCase 를 adminSignIn 가 성공했을때 자동으로 saveToken 하도록 수정

--- core/domain/src/main/java/com/school_of_company/domain/usecase/auth/AdminSignInRequestUseCase.kt
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/auth/AdminSignInRequestUseCase.kt
@@ -2,15 +2,18 @@ package com.school_of_company.domain.usecase.auth
 
 import com.school_of_company.data.repository.auth.AuthRepository
 import com.school_of_company.model.param.auth.AdminSignInRequestParam
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class AdminSignInRequestUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
-    suspend operator fun invoke(body: AdminSignInRequestParam) = runCatching {
-        // 로그인 처리
-        authRepository.adminSignIn(body).collect { token ->
-            authRepository.saveToken(token) // 로그인 후 토큰 저장
-        }
+    operator fun invoke(body: AdminSignInRequestParam): Flow<Unit> = flow {
+        emit(
+            authRepository.adminSignIn(body).collect { token ->
+                authRepository.saveToken(token)
+            }
+        )
     }
 }

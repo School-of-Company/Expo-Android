7bfa7630f6de70a2893bbf17c24b14cff985224a
명훈 <s23065@gsm.hs.kr>
2025-01-23 23:14:01 +0900

    :recycle: :: Apply Before Commit -> ModifyExpoInformationUseCase

--- core/domain/src/main/java/com/school_of_company/domain/usecase/expo/ModifyExpoInformationUseCase.kt
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/expo/ModifyExpoInformationUseCase.kt
@@ -2,6 +2,7 @@ package com.school_of_company.domain.usecase.expo
 
 import com.school_of_company.data.repository.expo.ExpoRepository
 import com.school_of_company.model.model.expo.ExpoRequestAndResponseModel
+import com.school_of_company.model.param.expo.ExpoAllRequestParam
 import javax.inject.Inject
 
 class ModifyExpoInformationUseCase @Inject constructor(
@@ -9,7 +10,7 @@ class ModifyExpoInformationUseCase @Inject constructor(
 ) {
     operator fun invoke(
         expoId: String,
-        body: ExpoRequestAndResponseModel
+        body: ExpoAllRequestParam
     ) = runCatching {
         repository.modifyExpoInformation(
             expoId = expoId,

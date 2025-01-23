cefa696ba3383505eadedf1d07bdd3e77dbba956
명훈 <s23065@gsm.hs.kr>
2025-01-24 04:57:10 +0900

    :fire: :: Fire Unused Files

--- core/model/src/main/java/com/school_of_company/model/model/standard/StandardRequestModel.kt
+++
@@ -1,7 +0,0 @@
-package com.school_of_company.model.model.standard
-
-data class StandardRequestModel(
-    val title: String,
-    val startedAt: String,
-    val endedAt: String,
-)
--- core/model/src/main/java/com/school_of_company/model/model/training/TrainingDtoModel.kt
+++
@@ -1,8 +0,0 @@
-package com.school_of_company.model.model.training
-
-data class TrainingDtoModel(
-    val title: String,
-    val startedAt: String,
-    val endedAt: String,
-    val category: String,
-)

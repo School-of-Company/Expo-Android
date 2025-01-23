d40dc92e853f1cc07752f6577fe2071be870239b
명훈 <s23065@gsm.hs.kr>
2025-01-23 23:14:23 +0900

    :recycle: :: Apply Before Commit -> RegisterExpoInformationUseCase

--- core/domain/src/main/java/com/school_of_company/domain/usecase/expo/RegisterExpoInformationUseCase.kt
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/expo/RegisterExpoInformationUseCase.kt
@@ -3,12 +3,13 @@ package com.school_of_company.domain.usecase.expo
 import com.school_of_company.data.repository.expo.ExpoRepository
 import com.school_of_company.model.entity.expo.ExpoIdResponseEntity
 import com.school_of_company.model.model.expo.ExpoRequestAndResponseModel
+import com.school_of_company.model.param.expo.ExpoAllRequestParam
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class RegisterExpoInformationUseCase @Inject constructor(
     private val repository: ExpoRepository
 ) {
-    operator fun invoke(body: ExpoRequestAndResponseModel): Flow<ExpoIdResponseEntity> =
+    operator fun invoke(body: ExpoAllRequestParam): Flow<ExpoIdResponseEntity> =
         repository.registerExpoInformation(body = body)
 }

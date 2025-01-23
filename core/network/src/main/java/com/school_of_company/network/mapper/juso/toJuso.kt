0d7b6b578f3064c1dbf046066691ae50cda6d30c
answad <mymhj122@gmail.com>
2025-01-23 20:40:47 +0900

    :sparkles: :: mapper toJuso 추가

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/juso/toJuso.kt
@@ -0,0 +1,6 @@
+package com.school_of_company.network.mapper.juso
+
+import com.school_of_company.model.model.juso.JusoModel
+import com.school_of_company.network.dto.juso.Juso
+
+fun Juso.toJuso() = JusoModel(roadAddr = roadAddr, jibunAddr = jibunAddr)

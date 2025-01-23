e91006ee098ecd781c97228b8f905cffee41c023
명훈 <s23065@gsm.hs.kr>
2025-01-24 05:04:01 +0900

    :lipstick: :: Add Component(AddTextField, SettingBottomSheet, StandardAddTextField, StandardSettingBottomSheet)

--- feature/expo/src/main/java/com/school_of_company/expo/view/component/ExpoSettingBottomSheet.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/view/component/ExpoSettingBottomSheet.kt
@@ -47,7 +47,7 @@ import com.school_of_company.design_system.icon.XIcon
 import com.school_of_company.design_system.theme.ExpoAndroidTheme
 import com.school_of_company.design_system.theme.color.ExpoColor
 import com.school_of_company.expo.enum.TrainingCategory
-import com.school_of_company.model.model.training.TrainingDtoModel
+import com.school_of_company.model.param.expo.TrainingProIdRequestParam
 import com.school_of_company.model.param.expo.TrainingProRequestParam
 import com.school_of_company.ui.keyBoardOption.numberKeyboardOptions
 import com.school_of_company.ui.util.filterNonDigits
@@ -57,7 +57,6 @@ import com.school_of_company.ui.visualTransformation.DateTimeVisualTransformatio
 @Composable
 internal fun ExpoSettingBottomSheet(
     modifier: Modifier = Modifier,
-    isTraining: Boolean = true,
     onCancelClick: () -> Unit,
     trainingSettingItem: TrainingProRequestParam,
     focusManager: FocusManager = LocalFocusManager.current,
@@ -136,7 +135,8 @@ internal fun ExpoSettingBottomSheet(
                                 )
                             },
                             onTextChange = { newText ->
-                                currentItem = currentItem.copy(startedAt = newText.filterNonDigits())
+                                currentItem =
+                                    currentItem.copy(startedAt = newText.filterNonDigits())
                             }
                         )
 
@@ -159,26 +159,22 @@ internal fun ExpoSettingBottomSheet(
                         )
                     }
 
-                    if (isTraining) {
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Text(
-                                text = "필수",
-                                style = typography.bodyRegular2,
-                                color = colors.gray500
-                            )
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "필수",
+                            style = typography.bodyRegular2,
+                            color = colors.gray500
+                        )
 
-                            CustomCheckBox(
-                                categoryState = currentItem.category,
-                                onCategoryChange = { newCategory ->
-                                    currentItem = currentItem.copy(category = newCategory)
-                                }
-                            )
-                        }
-                    } else {
-                        Spacer(modifier = Modifier.size(1.dp))
+                        CustomCheckBox(
+                            categoryState = currentItem.category,
+                            onCategoryChange = { newCategory ->
+                                currentItem = currentItem.copy(category = newCategory)
+                            }
+                        )
                     }
                 }
 
@@ -197,6 +193,150 @@ internal fun ExpoSettingBottomSheet(
                     modifier = Modifier.fillMaxWidth()
                 )
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun ExpoSettingModifyBottomSheet(
+    modifier: Modifier = Modifier,
+    onCancelClick: () -> Unit,
+    trainingSettingItem: TrainingProIdRequestParam,
+    focusManager: FocusManager = LocalFocusManager.current,
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    onButtonClick: () -> Unit,
+    onTrainingSettingChange: (TrainingProIdRequestParam) -> Unit,
+) {
+    var currentItem by remember { mutableStateOf(trainingSettingItem) }
+
+    ExpoAndroidTheme { colors, typography ->
+
+        ModalBottomSheet(
+            onDismissRequest = { onCancelClick() },
+            sheetState = sheetState,
+            containerColor = colors.white,
+            shape = RoundedCornerShape(
+                topStart = 6.dp,
+                topEnd = 6.dp
+            )
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.Top),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = colors.white,
+                        shape = RoundedCornerShape(
+                            topStart = 6.dp,
+                            topEnd = 6.dp
+                        )
+                    )
+                    .padding(
+                        horizontal = 18.dp,
+                        vertical = 24.dp
+                    )
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onTap = {
+                                focusManager.clearFocus()
+                            }
+                        )
+                    }
+                    .imePadding()
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "연수 설정",
+                        style = typography.titleBold3,
+                        color = colors.black,
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    XIcon(
+                        tint = colors.black,
+                        modifier = Modifier.expoClickable { onCancelClick() }
+                    )
+                }
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Top),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        ExpoNoneLineTextField(
+                            textState = currentItem.startedAt,
+                            lengthLimit = 12,
+                            keyboardOptions = numberKeyboardOptions(),
+                            visualTransformation = DateTimeVisualTransformation(),
+                            placeHolder = {
+                                Text(
+                                    text = "yyyy-MM-dd HH:mm",
+                                    style = typography.titleBold2,
+                                    color = colors.gray300,
+                                    maxLines = 1
+                                )
+                            },
+                            onTextChange = { newText ->
+                                currentItem =
+                                    currentItem.copy(startedAt = newText.filterNonDigits())
+                            }
+                        )
+
+                        ExpoNoneLineTextField(
+                            textState = currentItem.endedAt,
+                            lengthLimit = 12,
+                            keyboardOptions = numberKeyboardOptions(),
+                            visualTransformation = DateTimeVisualTransformation(),
+                            placeHolder = {
+                                Text(
+                                    text = "yyyy-MM-dd HH:mm",
+                                    style = typography.titleBold2,
+                                    color = colors.gray300,
+                                    maxLines = 1
+                                )
+                            },
+                            onTextChange = { newText ->
+                                currentItem = currentItem.copy(endedAt = newText.filterNonDigits())
+                            }
+                        )
+                    }
+
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "필수",
+                            style = typography.bodyRegular2,
+                            color = colors.gray500
+                        )
+
+                        CustomCheckBox(
+                            categoryState = currentItem.category,
+                            onCategoryChange = { newCategory ->
+                                currentItem = currentItem.copy(category = newCategory)
+                            }
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+
+            ExpoStateButton(
+                text = "확인",
+                onClick = {
+                    if (currentItem.startedAt.isValidDateTimeSequence(currentItem.endedAt)) {
+                        onTrainingSettingChange(currentItem)
+                        onButtonClick()
+                    } else {
+                        // TODO: 예외 처리
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }

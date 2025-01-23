471d3bd37742c6e34530b4221be5e91704a2da08
명훈 <s23065@gsm.hs.kr>
2025-01-24 04:55:20 +0900

    :sparkles: :: Create ExpoModifyRequestParam

---
+++ core/model/src/main/java/com/school_of_company/model/param/expo/ExpoModifyRequestParam.kt
@@ -0,0 +1,29 @@
+package com.school_of_company.model.param.expo
+
+data class ExpoModifyRequestParam(
+    val title: String,
+    val description: String,
+    val startedDay: String, // yyyy-mm-dd
+    val finishedDay: String, // yyyy-mm-dd
+    val location: String,
+    val coverImage: String?,
+    val x: Float,
+    val y: Float,
+    val updateStandardProRequestDto: List<StandardProIdRequestParam>,
+    val updateTrainingProRequestDto: List<TrainingProIdRequestParam>,
+)
+
+data class StandardProIdRequestParam(
+    val id: Long,
+    val title: String,
+    val startedAt: String, // yyyy-MM-DD HH:mm
+    val endedAt: String, // yyyy-MM-DD HH:mm
+)
+
+data class TrainingProIdRequestParam(
+    val id: Long,
+    val title: String,
+    val startedAt: String, // yyyy-MM-dd HH:mm
+    val endedAt: String, // yyyy-MM-dd HH:mm
+    val category: String, // ESSENTIAL, CHOICE
+)

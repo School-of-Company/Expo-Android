b4ba64bc693d8fb77b069d5bbd60753e2a169c35
answad <mymhj122@gmail.com>
2025-01-23 16:44:36 +0900

    :sparkles: :: useCase의 반환값을 강제로 Unit으로 제한하지 않고 반환값을 정상 반환하도록 수정

--- core/domain/src/main/java/com/school_of_company/domain/usecase/auth/AdminTokenRefreshUseCase.kt
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/auth/AdminTokenRefreshUseCase.kt
@@ -1,17 +1,17 @@
 package com.school_of_company.domain.usecase.auth
 
 import com.school_of_company.data.repository.auth.AuthRepository
+import com.school_of_company.model.model.auth.AdminTokenResponseModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class AdminTokenRefreshUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
-    operator fun invoke(): Flow<Unit> =
+    operator fun invoke(): Flow<AdminTokenResponseModel> =
         authRepository.adminTokenRefresh()
             .onEach { tokenResponse ->
                 authRepository.saveToken(tokenResponse)
-            }.map { Unit }
+            }
 }

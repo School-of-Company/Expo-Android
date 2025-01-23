3a474b3a39e9729257a45b655a48b6e4fd61835c
명훈 <s23065@gsm.hs.kr>
2025-01-23 23:09:40 +0900

    :sparkles: :: Create ExpoAllRequestParam

---
+++ core/model/src/main/java/com/school_of_company/model/param/expo/ExpoAllRequestParam.kt
@@ -0,0 +1,27 @@
+package com.school_of_company.model.param.expo
+
+data class ExpoAllRequestParam(
+    val title: String,
+    val description: String,
+    val startedDay: String, // yyyy-mm-dd
+    val finishedDay: String, // yyyy-mm-dd
+    val location: String,
+    val coverImage: String?,
+    val x: Float,
+    val y: Float,
+    val addStandardProRequestDto: List<StandardProRequestParam>,
+    val addTrainingProRequestDto: List<TrainingProRequestParam>
+)
+
+data class StandardProRequestParam(
+    val title: String,
+    val startedAt: String, // yyyy-MM-DD HH:mm
+    val endedAt: String // yyyy-MM-DD HH:mm
+)
+
+data class TrainingProRequestParam(
+    val title: String,
+    val startedAt: String, // yyyy-MM-dd HH:mm
+    val endedAt: String, // yyyy-MM-dd HH:mm
+    val category: String // ESSENTIAL, CHOICE
+)

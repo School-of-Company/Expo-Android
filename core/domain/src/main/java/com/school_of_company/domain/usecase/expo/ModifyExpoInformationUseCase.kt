d2ab97dfec1480826ef8dd5d18af4326d8ec746f
명훈 <s23065@gsm.hs.kr>
2025-01-24 05:05:04 +0900

    :recycle: :: Apply Changed Param

--- core/domain/src/main/java/com/school_of_company/domain/usecase/expo/ModifyExpoInformationUseCase.kt
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/expo/ModifyExpoInformationUseCase.kt
@@ -3,6 +3,7 @@ package com.school_of_company.domain.usecase.expo
 import com.school_of_company.data.repository.expo.ExpoRepository
 import com.school_of_company.model.model.expo.ExpoRequestAndResponseModel
 import com.school_of_company.model.param.expo.ExpoAllRequestParam
+import com.school_of_company.model.param.expo.ExpoModifyRequestParam
 import javax.inject.Inject
 
 class ModifyExpoInformationUseCase @Inject constructor(
@@ -10,7 +11,7 @@ class ModifyExpoInformationUseCase @Inject constructor(
 ) {
     operator fun invoke(
         expoId: String,
-        body: ExpoAllRequestParam
+        body: ExpoModifyRequestParam
     ) = runCatching {
         repository.modifyExpoInformation(
             expoId = expoId,

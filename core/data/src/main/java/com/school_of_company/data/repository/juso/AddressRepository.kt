34a67fa3d1429acf01ef8c870c333fdf53dbaef7
answad <mymhj122@gmail.com>
2025-01-23 20:38:45 +0900

    :sparkles: :: AddressRepository 추가

---
+++ core/data/src/main/java/com/school_of_company/data/repository/juso/AddressRepository.kt
@@ -0,0 +1,12 @@
+package com.school_of_company.data.repository.juso
+
+import com.school_of_company.model.model.juso.JusoModel
+import kotlinx.coroutines.flow.Flow
+
+interface AddressRepository {
+    suspend fun getAddress(
+        currentPage: Int,
+        countPerPage: Int,
+        keyword: String,
+    ): Flow<List<JusoModel>>
+}
---
+++ core/data/src/main/java/com/school_of_company/data/repository/juso/AddressRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.school_of_company.data.repository.juso
+
+import com.school_of_company.model.model.juso.JusoModel
+import com.school_of_company.network.datasource.juso.AddressDataSource
+import com.school_of_company.network.mapper.juso.toJuso
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class AddressRepositoryImpl @Inject constructor(
+    private val addressDataSource: AddressDataSource
+) : AddressRepository {
+    override suspend fun getAddress(
+        currentPage: Int,
+        countPerPage: Int,
+        keyword: String
+    ): Flow<List<JusoModel>> =
+        addressDataSource.getAddress(
+            countPerPage = countPerPage,
+            currentPage = currentPage,
+            keyword = keyword
+        ).map {
+            it.results.juso?.map { juso -> juso.toJuso() } ?: emptyList()
+        }
+}

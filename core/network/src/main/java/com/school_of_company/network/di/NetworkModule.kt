31c4c98541c3b9784d41e04ca9e9054bd6bd49ca
answad <mymhj122@gmail.com>
2025-01-24 10:42:38 +0900

    :recycle: :: 각기 다른 Base URL을 처리할 수 있도록 Retrofit 인스턴스에 @BaseUrl, @KakaoUrl, @AddressUrl 어노테이션 추가, AuthAPI, SmsAPI, ExpoAPI 등에서 각기 다른 Base URL을 사용하는 Retrofit 인스턴스 적용 NetworkModule에서 각 서비스에 맞는 Retrofit 클라이언트를 제공하도록 리팩토링

--- core/network/src/main/java/com/school_of_company/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/school_of_company/network/di/NetworkModule.kt
@@ -10,12 +10,16 @@ import com.school_of_company.network.api.AttendanceAPI
 import com.school_of_company.network.api.AuthAPI
 import com.school_of_company.network.api.ExpoAPI
 import com.school_of_company.network.api.ImageAPI
+import com.school_of_company.network.api.KakaoLocalApi
 import com.school_of_company.network.api.ParticipantAPI
 import com.school_of_company.network.api.SmsAPI
 import com.school_of_company.network.api.StandardAPI
 import com.school_of_company.network.api.TraineeAPI
 import com.school_of_company.network.api.TrainingAPI
+import com.school_of_company.network.util.AddressUrl
 import com.school_of_company.network.util.AuthInterceptor
+import com.school_of_company.network.util.BaseUrl
+import com.school_of_company.network.util.KakaoUrl
 import com.school_of_company.network.util.TokenAuthenticator
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -36,6 +40,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
     @Provides
     fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
         HttpLoggingInterceptor { message -> Log.v("Http", message) }
@@ -51,7 +56,7 @@ object NetworkModule {
         httpLoggingInterceptor: HttpLoggingInterceptor,
         authInterceptor: AuthInterceptor,
         tokenAuthenticator: TokenAuthenticator,
-    ) : OkHttpClient {
+    ): OkHttpClient {
         return OkHttpClient.Builder()
             .cookieJar(CookieJar.NO_COOKIES)
             .connectTimeout(30, TimeUnit.SECONDS)
@@ -66,7 +71,7 @@ object NetworkModule {
                         .maxContentLength(250000)
                         .retainDataFor(ChuckInterceptor.Period.ONE_DAY)
                 } else {
-                    Interceptor {chain -> chain.proceed(chain.request()) }
+                    Interceptor { chain -> chain.proceed(chain.request()) }
                 }
             )
             .authenticator(tokenAuthenticator)
@@ -75,22 +80,23 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideMoshiInstance() : Moshi {
+    fun provideMoshiInstance(): Moshi {
         return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
     }
 
     @Provides
     @Singleton
-    fun provideMoshiConverterFactory(moshi: Moshi) : MoshiConverterFactory {
+    fun provideMoshiConverterFactory(moshi: Moshi): MoshiConverterFactory {
         return MoshiConverterFactory.create(moshi)
     }
 
     @Provides
     @Singleton
-    fun provideRetrofitInstance(
+    @BaseUrl
+    fun provideBaseApiRetrofit(
         okHttpClient: OkHttpClient,
         moshiConverterFactory: MoshiConverterFactory
-    ) : Retrofit {
+    ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .client(okHttpClient)
@@ -99,50 +105,79 @@ object NetworkModule {
     }
 
     @Provides
-    fun provideAuthAPI(retrofit: Retrofit) : AuthAPI =
+    @Singleton
+    @KakaoUrl
+    fun provideKakaoApiRetrofit(
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.KAKAO_URL)
+            .client(okHttpClient)
+            .addConverterFactory(moshiConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @AddressUrl
+    fun provideAddressApiRetrofit(
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.ADDRESS_URL)
+            .client(okHttpClient)
+            .addConverterFactory(moshiConverterFactory)
+            .build()
+    }
+
+    @Provides
+    fun provideAuthAPI(@BaseUrl retrofit: Retrofit): AuthAPI =
         retrofit.create(AuthAPI::class.java)
 
     @Provides
-    fun provideSmsAPI(retrofit: Retrofit) : SmsAPI =
+    @Singleton
+    fun provideSmsAPI(@BaseUrl retrofit: Retrofit): SmsAPI =
         retrofit.create(SmsAPI::class.java)
 
     @Provides
-    fun provideExpoAPI(retrofit: Retrofit) : ExpoAPI =
+    fun provideExpoAPI(@BaseUrl retrofit: Retrofit): ExpoAPI =
         retrofit.create(ExpoAPI::class.java)
 
     @Provides
-    fun provideImageAPI(retrofit: Retrofit) : ImageAPI =
+    fun provideImageAPI(@BaseUrl retrofit: Retrofit): ImageAPI =
         retrofit.create(ImageAPI::class.java)
 
     @Provides
-    fun provideTrainingAPI(retrofit: Retrofit) : TrainingAPI =
+    fun provideTrainingAPI(@BaseUrl retrofit: Retrofit): TrainingAPI =
         retrofit.create(TrainingAPI::class.java)
 
     @Provides
-    fun provideStandardAPI(retrofit: Retrofit) : StandardAPI =
+    fun provideStandardAPI(@BaseUrl retrofit: Retrofit): StandardAPI =
         retrofit.create(StandardAPI::class.java)
 
     @Provides
-    fun provideAttendanceAPI(retrofit: Retrofit) : AttendanceAPI =
+    fun provideAttendanceAPI(@BaseUrl retrofit: Retrofit): AttendanceAPI =
         retrofit.create(AttendanceAPI::class.java)
 
     @Provides
-    fun provideAdminAPI(retrofit: Retrofit) : AdminAPI =
+    fun provideAdminAPI(@BaseUrl retrofit: Retrofit): AdminAPI =
         retrofit.create(AdminAPI::class.java)
 
     @Provides
-    fun provideTraineeAPI(retrofit: Retrofit) : TraineeAPI =
+    fun provideTraineeAPI(@BaseUrl retrofit: Retrofit): TraineeAPI =
         retrofit.create(TraineeAPI::class.java)
 
     @Provides
-    fun provideParticipantApi(retrofit: Retrofit) : ParticipantAPI =
+    fun provideParticipantApi(@BaseUrl retrofit: Retrofit): ParticipantAPI =
         retrofit.create(ParticipantAPI::class.java)
 
     @Provides
-    fun provideAddressApi(retrofit: Retrofit) : AddressApi =
-        retrofit.create(AddressApi::class.java)
+    fun provideKakaoLocalApi(@KakaoUrl retrofit: Retrofit): KakaoLocalApi =
+        retrofit.create(KakaoLocalApi::class.java)
 
     @Provides
-    fun provideKakaoLocalApi(retrofit: Retrofit): KakaoLocalApi =
-        retrofit.create(KakaoLocalApi::class.java)
+    fun provideAddressApi(@AddressUrl retrofit: Retrofit): AddressApi =
+        retrofit.create(AddressApi::class.java)
 }

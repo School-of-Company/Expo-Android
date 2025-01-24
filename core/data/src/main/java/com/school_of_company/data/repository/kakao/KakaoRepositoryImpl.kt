19bafd9e2c4120b736f16687fecf413a2d9c3c24
answad <mymhj122@gmail.com>
2025-01-24 10:17:11 +0900

    :sparkles: :: KakaoRepository 추가

---
+++ core/data/src/main/java/com/school_of_company/data/repository/kakao/KakaoRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.school_of_company.data.repository.kakao
+
+import com.school_of_company.model.model.kakao.KakaoAddressModel
+import com.school_of_company.network.datasource.kakao.KakaoLocalDataSource
+import com.school_of_company.network.mapper.kakao.toModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class KakaoRepositoryImpl @Inject constructor(
+    private val kakaoLocalDataSource: KakaoLocalDataSource,
+) : KakaoRepository {
+    override suspend fun getCoordinates(address: String): Flow<KakaoAddressModel> {
+        return kakaoLocalDataSource.getCoordinates(address = address).map { it.toModel() }
+    }
+}

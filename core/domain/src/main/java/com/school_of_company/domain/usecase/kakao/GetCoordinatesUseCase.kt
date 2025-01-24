03c0b0e108b04ba36bf55f4a8381d108f0f91606
answad <mymhj122@gmail.com>
2025-01-24 10:17:34 +0900

    :sparkles: :: GetCoordinatesUseCase 추가

---
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/kakao/GetCoordinatesUseCase.kt
@@ -0,0 +1,13 @@
+package com.school_of_company.domain.usecase.kakao
+
+import com.school_of_company.data.repository.kakao.KakaoRepository
+import com.school_of_company.model.model.kakao.KakaoAddressModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetCoordinatesUseCase @Inject constructor(
+    private val repository: KakaoRepository
+) {
+    suspend operator fun invoke(address: String): Flow<KakaoAddressModel> =
+        repository.getCoordinates(address = address)
+}

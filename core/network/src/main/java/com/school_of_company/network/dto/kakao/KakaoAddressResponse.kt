eeb9255cd9630486ac6c89bfae435f6bf3a6ce5a
answad <mymhj122@gmail.com>
2025-01-24 10:18:07 +0900

    :sparkles: :: Address api에서 사용하는 dataClass 추가

---
+++ core/network/src/main/java/com/school_of_company/network/dto/kakao/KakaoAddressResponse.kt
@@ -0,0 +1,20 @@
+package com.school_of_company.network.dto.kakao
+
+import com.squareup.moshi.Json
+
+data class KakaoAddressResponse(
+    @Json(name = "meta") val meta: Meta,
+    @Json(name = "documents") val documents: List<Document>
+)
+
+data class Meta(
+    @Json(name = "total_count") val totalCount: Int,
+    @Json(name = "pageable_count") val pageableCount: Int,
+    @Json(name = "is_end") val isEnd: Boolean
+)
+
+data class Document(
+    @Json(name = "address_name") val addressName: String,
+    @Json(name = "x") val x: String, // 경도
+    @Json(name = "y") val y: String // 위도
+)

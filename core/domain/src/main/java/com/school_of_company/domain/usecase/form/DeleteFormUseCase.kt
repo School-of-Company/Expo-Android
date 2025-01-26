89fc1d990a1185444c4544e7c7083fbe7898bdbd
명훈 <s23065@gsm.hs.kr>
2025-01-27 04:14:23 +0900

    :sparkles: :: Create DeleteFormUseCase

---
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/form/DeleteFormUseCase.kt
@@ -0,0 +1,12 @@
+package com.school_of_company.domain.usecase.form
+
+import com.school_of_company.data.repository.form.FormRepository
+import javax.inject.Inject
+
+class DeleteFormUseCase @Inject constructor(
+    private val repository: FormRepository
+) {
+    operator fun invoke(formId: Long) = runCatching {
+        repository.deleteForm(formId = formId)
+    }
+}

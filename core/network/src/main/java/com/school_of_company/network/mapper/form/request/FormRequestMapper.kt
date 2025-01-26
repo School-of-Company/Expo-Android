a69eab8f2f5416f8850128868c15bf3597af6bb2
명훈 <s23065@gsm.hs.kr>
2025-01-27 03:50:08 +0900

    :sparkles: :: Create FormRequestMapper

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/form/request/FormRequestMapper.kt
@@ -0,0 +1,20 @@
+package com.school_of_company.network.mapper.form.request
+
+import com.school_of_company.model.model.form.DynamicFormModel
+import com.school_of_company.model.model.form.FormRequestAndResponseModel
+import com.school_of_company.network.dto.form.all.DynamicForm
+import com.school_of_company.network.dto.form.all.FormRequestAndResponse
+
+fun FormRequestAndResponseModel.toModel(): FormRequestAndResponse =
+    FormRequestAndResponse(
+        informationImage = this.informationImage,
+        participantType = this.participantType,
+        dynamicForm = this.dynamicForm.map { it.toModel() }
+    )
+
+fun DynamicFormModel.toModel(): DynamicForm =
+    DynamicForm(
+        title = this.title,
+        formType = this.formType,
+        jsonData = this.jsonData
+    )

cbef04580d93d1019ad0e261e33ffa09936d7c16
youloveli <mymhj122@gmail.com>
2025-01-31 19:26:06 +0900

    :sparkles: :: JsonClass 어노테이션 추가

--- core/network/src/main/java/com/school_of_company/network/dto/kakao/KakaoAddressResponse.kt
+++ core/network/src/main/java/com/school_of_company/network/dto/kakao/KakaoAddressResponse.kt
@@ -1,18 +1,22 @@
 package com.school_of_company.network.dto.kakao
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class KakaoAddressResponse(
     @Json(name = "meta") val meta: Meta,
     @Json(name = "documents") val documents: List<Document>
 )
 
+@JsonClass(generateAdapter = true)
 data class Meta(
     @Json(name = "total_count") val totalCount: Int,
     @Json(name = "pageable_count") val pageableCount: Int,
     @Json(name = "is_end") val isEnd: Boolean
 )
 
+@JsonClass(generateAdapter = true)
 data class Document(
     @Json(name = "address_name") val addressName: String,
     @Json(name = "x") val x: String, // 경도

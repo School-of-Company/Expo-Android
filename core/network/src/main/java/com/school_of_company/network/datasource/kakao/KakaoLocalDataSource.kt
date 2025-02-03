0a2a7db86aea433bec9811d21631be2f58df0721
answad <mymhj122@gmail.com>
2025-02-03 19:32:00 +0900

    :sparkles: :: getAddress 함수의 반환값을 KakaoGeocodingResponse 에서 KakaoGetAddressResponse 으로 변경

--- core/network/src/main/java/com/school_of_company/network/datasource/kakao/KakaoLocalDataSource.kt
+++ core/network/src/main/java/com/school_of_company/network/datasource/kakao/KakaoLocalDataSource.kt
@@ -1,10 +1,10 @@
 package com.school_of_company.network.datasource.kakao
 
-import com.school_of_company.network.dto.kakao.KakaoGeocodingResponse
+import com.school_of_company.network.dto.kakao.KakaoGetAddressResponse
 import com.school_of_company.network.dto.kakao.KakaoGetCoordinatesResponse
 import kotlinx.coroutines.flow.Flow
 
 interface KakaoLocalDataSource {
-    fun getAddress(x: String, y: String): Flow<KakaoGeocodingResponse>
+    fun getAddress(x: String, y: String): Flow<KakaoGetAddressResponse>
     fun getCoordinates(address: String, size: Int): Flow<KakaoGetCoordinatesResponse>
 }

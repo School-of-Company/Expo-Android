837fd610404091ed71c5b9204d30bc78d3e0b6c7
answad <mymhj122@gmail.com>
2025-02-03 19:13:19 +0900

    :sparkles: :: getAddress api 추가

--- core/network/src/main/java/com/school_of_company/network/api/KakaoAPI.kt
+++ core/network/src/main/java/com/school_of_company/network/api/KakaoAPI.kt
@@ -1,6 +1,7 @@
 package com.school_of_company.network.api
 
 import com.school_of_company.network.dto.kakao.KakaoAddressResponse
+import com.school_of_company.network.dto.kakao.KakaoGeocodingResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -14,4 +15,12 @@ interface KakaoAPI {
         @Query("query") address: String,
         @Query("size") size: Int,
     ): KakaoAddressResponse
+
+    @GET("geo/coord2address.json")
+    suspend fun getAddress(
+        @Header("Authorization") apiKey: String,
+        @Header("Content-Type") contentType: String = "application/json;charset=UTF-8",
+        @Query("x") x: String,
+        @Query("y") y: String,
+    ): KakaoGeocodingResponse
 }

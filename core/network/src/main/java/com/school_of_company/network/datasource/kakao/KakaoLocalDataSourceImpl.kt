db402a8b0e0526e0ec3ad8c4b63e290094556588
answad <mymhj122@gmail.com>
2025-02-04 13:36:18 +0900

    :recycle: :: KakaoLocalDataSource 에서의 apiKey를 넘겨주는 부분 삭제

--- core/network/src/main/java/com/school_of_company/network/datasource/kakao/KakaoLocalDataSourceImpl.kt
+++ core/network/src/main/java/com/school_of_company/network/datasource/kakao/KakaoLocalDataSourceImpl.kt
@@ -1,9 +1,8 @@
 package com.school_of_company.network.datasource.kakao
 
-import com.school_of_company.network.BuildConfig
 import com.school_of_company.network.api.KakaoAPI
-import com.school_of_company.network.dto.kakao.KakaoGetCoordinatesResponse
 import com.school_of_company.network.dto.kakao.KakaoGetAddressResponse
+import com.school_of_company.network.dto.kakao.KakaoGetCoordinatesResponse
 import com.school_of_company.network.util.performApiRequest
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -14,7 +13,6 @@ class KakaoLocalDataSourceImpl @Inject constructor(
     override fun getCoordinates(address: String, size: Int): Flow<KakaoGetCoordinatesResponse> =
         performApiRequest {
             service.getCoordinates(
-                apiKey = "KakaoAK ${BuildConfig.KAKAO_REST_KEY}",
                 address = address,
                 size = size,
             )
@@ -23,7 +21,6 @@ class KakaoLocalDataSourceImpl @Inject constructor(
     override fun getAddress(x: String, y: String): Flow<KakaoGetAddressResponse> =
         performApiRequest {
             service.getAddress(
-                apiKey = "KakaoAK ${BuildConfig.KAKAO_REST_KEY}",
                 x = x,
                 y = y,
             )

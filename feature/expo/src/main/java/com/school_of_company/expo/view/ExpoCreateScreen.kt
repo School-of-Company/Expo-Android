c98b39960e95668cde7dfbd04e848b7e9f3b3f9b
answad <mymhj122@gmail.com>
2025-02-05 19:37:00 +0900

    :recycle: :: 액스포 생성시에 좌표를 최대 6자리까지만 보내도록 수정

--- feature/expo/src/main/java/com/school_of_company/expo/view/ExpoCreateScreen.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/view/ExpoCreateScreen.kt
@@ -140,8 +140,8 @@ internal fun ExpoCreateRoute(
                         description = viewModel.introduce_title.value,
                         location = viewModel.location.value,
                         coverImage = (imageUpLoadUiState as ImageUpLoadUiState.Success).data.imageURL,
-                        x = viewModel.coordinateX.value,
-                        y = viewModel.coordinateY.value,
+                        x = viewModel.coordinateX.value.toDoubleOrNull()?.let { "%.6f".format(it) } ?: "0.000000",
+                        y = viewModel.coordinateY.value.toDoubleOrNull()?.let { "%.6f".format(it) } ?: "0.000000",
                         addStandardProRequestDto = standardProgramTextState,
                         addTrainingProRequestDto = trainingProgramTextState
                     )

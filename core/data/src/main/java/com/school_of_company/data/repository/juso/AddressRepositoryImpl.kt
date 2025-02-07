a402b9c8682b302ad81aa69402de3ef9d20f8f14
youloveli <mymhj122@gmail.com>
2025-02-07 15:36:04 +0900

    :recycle: :: 불필요한 import 삭제

--- core/data/src/main/java/com/school_of_company/data/repository/juso/AddressRepositoryImpl.kt
+++ core/data/src/main/java/com/school_of_company/data/repository/juso/AddressRepositoryImpl.kt
@@ -4,7 +4,6 @@ import com.school_of_company.model.model.juso.JusoModel
 import com.school_of_company.network.datasource.juso.AddressDataSource
 import com.school_of_company.network.mapper.juso.toModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 

9c7d028ec03b3aca70a85146a64093f4c4b4473f
youloveli <mymhj122@gmail.com>
2025-02-11 16:49:36 +0900

    :lipstick: :: FormAddButton 퍼블리싱, preview 추가

---
+++ feature/form/src/main/java/com/school_of_company/form/view/component/FormAddButton.kt
@@ -0,0 +1,63 @@
+package com.school_of_company.form.view.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.school_of_company.design_system.component.modifier.clickable.expoClickable
+import com.school_of_company.design_system.icon.PlusIcon
+import com.school_of_company.design_system.theme.ExpoAndroidTheme
+
+@Composable
+fun FormAddButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    ExpoAndroidTheme { colors, typography ->
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .expoClickable(onClick = onClick)
+                .border(
+                    width = 1.dp,
+                    color = colors.main400,
+                    shape = RoundedCornerShape(size = 6.dp),
+                )
+                .background(
+                    color = colors.white,
+                    shape = RoundedCornerShape(size = 6.dp),
+                )
+                .padding(vertical = 8.dp, horizontal = 12.dp),
+        ) {
+            PlusIcon(tint = colors.main400)
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = "추가하기",
+                style = typography.bodyBold2,
+                fontWeight = FontWeight.W600,
+                color = colors.main400,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun FormAddButtonPreview() {
+    FormAddButton(
+        onClick = { },
+    )
+}

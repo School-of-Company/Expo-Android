77954eb5ef8024d39ecb9a93d1494d565e32e44a
youloveli <mymhj122@gmail.com>
2025-02-13 22:57:32 +0900

    :fire: :: DynamicFormViewData 에서 itemList 삭제

--- feature/form/src/main/java/com/school_of_company/form/viewModel/viewData/DynamicFormViewData.kt
+++ feature/form/src/main/java/com/school_of_company/form/viewModel/viewData/DynamicFormViewData.kt
@@ -5,7 +5,6 @@ import com.school_of_company.form.enum.FormType
 data class DynamicFormViewData(
     val title: String,
     val formType: FormType, // SENTENCE, CHECKBOX, DROPDOWN, IMAGE, MULTIPLE
-    val itemList: List<String>,
     val requiredStatus: Boolean,
     val otherJson: Boolean,
 )

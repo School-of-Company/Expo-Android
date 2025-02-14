5e737d315b3128584c8dc1b5928456694c2fd95e
youloveli <mymhj122@gmail.com>
2025-02-14 16:06:35 +0900

    :recycle: :: DynamicFormViewData 에 itemList 추가

--- feature/form/src/main/java/com/school_of_company/form/viewModel/viewData/DynamicFormViewData.kt
+++ feature/form/src/main/java/com/school_of_company/form/viewModel/viewData/DynamicFormViewData.kt
@@ -5,6 +5,7 @@ import com.school_of_company.form.enum.FormType
 data class DynamicFormViewData(
     val title: String,
     val formType: FormType, // SENTENCE, CHECKBOX, DROPDOWN, IMAGE, MULTIPLE
+    val itemList: List<String>,
     val requiredStatus: Boolean,
     val otherJson: Boolean,
 )

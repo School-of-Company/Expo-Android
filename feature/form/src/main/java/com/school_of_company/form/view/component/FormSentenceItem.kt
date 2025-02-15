c80f7277dc14f535b04b3d7c66e8bf8f12c2fdbf
youloveli <mymhj122@gmail.com>
2025-02-15 17:11:16 +0900

    :lipstick: :: FormSentenceItem 의 텍스트 변경

--- feature/form/src/main/java/com/school_of_company/form/view/component/FormSentenceItem.kt
+++ feature/form/src/main/java/com/school_of_company/form/view/component/FormSentenceItem.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,7 +14,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.school_of_company.design_system.component.modifier.clickable.expoClickable
-import com.school_of_company.design_system.icon.ImageIcon
 import com.school_of_company.design_system.icon.XIcon
 import com.school_of_company.design_system.theme.ExpoAndroidTheme
 
@@ -33,7 +31,7 @@ internal fun FormSentenceItem(
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Text(
-                text = "업로드",
+                text = "문장을 입력해주세요.",
                 style = typography.captionRegular1,
                 fontWeight = FontWeight.W400,
                 color = colors.black,

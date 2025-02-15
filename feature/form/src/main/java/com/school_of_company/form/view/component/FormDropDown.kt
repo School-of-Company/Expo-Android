d5efdf7f87ea2a8a1eeade4a9593acebcf5b71db
youloveli <mymhj122@gmail.com>
2025-02-15 15:52:28 +0900

    :recycle: :: FormDropDown 의 expended 상태를 rememberSaveable 로 관리하도록 수정

--- feature/form/src/main/java/com/school_of_company/form/view/component/FormDropDown.kt
+++ feature/form/src/main/java/com/school_of_company/form/view/component/FormDropDown.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -36,7 +37,7 @@ internal fun FormDropDown(
     currentItem: FormType,
     onItemClick: (FormType) -> Unit,
 ) {
-    val expanded = remember { mutableStateOf(false) }
+    val expanded = rememberSaveable { mutableStateOf(false) }
 
     ExpoAndroidTheme { colors, typography ->
         Column(modifier = modifier) {

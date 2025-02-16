d9db466129f9479082afd84c7115a8f99e4cc4ce
answad <mymhj122@gmail.com>
2025-02-16 18:35:00 +0900

    :sparkles: :: convertJsonMapToList 함수 추가

--- core/network/src/main/java/com/school_of_company/network/util/convertListToJsonMap.kt
+++ core/network/src/main/java/com/school_of_company/network/util/convertListToJsonMap.kt
@@ -1,9 +1,20 @@
 package com.school_of_company.network.util
 
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 
 val moshi: Moshi = Moshi.Builder().build()
-val jsonAdapter = moshi.adapter(Map::class.java)
 
-fun List<String>.convertListToJsonMap(): String =
-    jsonAdapter.toJson(this.mapIndexed { index, value -> "${index + 1}" to value }.toMap())
+fun List<String>.convertListToJsonMap(): String {
+    val jsonAdapter = moshi.adapter(Map::class.java)
+
+    return jsonAdapter.toJson(this.mapIndexed { index, value -> "${index + 1}" to value }.toMap())
+}
+
+fun String.convertJsonMapToList(): List<String> {
+    val type = Types.newParameterizedType(Map::class.java, String::class.java, String::class.java)
+    val jsonAdapter = moshi.adapter<Map<String, String>>(type)
+
+    val map = jsonAdapter.fromJson(this) ?: return emptyList()
+    return map.values.toList()
+}

ab1004890594d68e0f95211bce021e949ee259b0
answad <mymhj122@gmail.com>
2025-02-24 15:53:18 +0900

    :sparkles: :: GetFormUiState 생성

--- feature/form/src/main/java/com/school_of_company/form/viewModel/uiState/GetFormUiState.kt
+++ feature/form/src/main/java/com/school_of_company/form/viewModel/uiState/GetFormUiState.kt
@@ -1,9 +1,7 @@
 package com.school_of_company.form.viewModel.uiState
 
-import com.school_of_company.model.model.form.DynamicFormModel
-
 internal sealed interface GetFormUiState {
     object Loading : GetFormUiState
-    data class Success(val list: List<DynamicFormModel>) : GetFormUiState
+    object Success : GetFormUiState
     data class Error(val exception: Throwable) : GetFormUiState
 }

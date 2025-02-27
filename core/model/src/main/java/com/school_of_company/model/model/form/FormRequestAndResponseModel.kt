f452bc3a924891f7ead68b33d93c73c63d20607d
answad <mymhj122@gmail.com>
2025-02-27 16:44:01 +0900

    :fire: :: 주석 삭제

--- core/model/src/main/java/com/school_of_company/model/model/form/FormRequestAndResponseModel.kt
+++ core/model/src/main/java/com/school_of_company/model/model/form/FormRequestAndResponseModel.kt
@@ -3,7 +3,7 @@ package com.school_of_company.model.model.form
 data class FormRequestAndResponseModel(
     val informationImage: String,
     val participantType: String, // TRAINEE, STANDARD
-    val dynamicForm: List<DynamicFormModel>
+    val dynamicForm: List<DynamicFormModel>,
 )
 
 data class DynamicFormModel(
@@ -16,10 +16,10 @@ data class DynamicFormModel(
     companion object {
         fun createDefault(): DynamicFormModel = DynamicFormModel(
             title = "",
-            formType = "SENTENCE",  // 기본 폼 타입 지정 (필요에 따라 변경 가능)
-            itemList = listOf(""),  // 기본 아이템 리스트
-            requiredStatus = false, // 기본 필수 여부
-            otherJson = false       // 기본 기타 JSON 여부
+            formType = "SENTENCE",
+            itemList = listOf(""),
+            requiredStatus = false,
+            otherJson = false,
         )
     }
 }

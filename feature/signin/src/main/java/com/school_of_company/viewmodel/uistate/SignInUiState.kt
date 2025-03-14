d6d3dd61e48790152b9a1328ca78640746afab73
명훈 <s23065@gsm.hs.kr>
2025-03-14 16:59:09 +0900

    :recycle: :: Refactor SignInUiState

--- feature/signin/src/main/java/com/school_of_company/viewmodel/uistate/SignInUiState.kt
+++ feature/signin/src/main/java/com/school_of_company/viewmodel/uistate/SignInUiState.kt
@@ -3,9 +3,13 @@ package com.school_of_company.signin.viewmodel.uistate
 sealed interface SignInUiState {
     object Loading : SignInUiState
     object Success : SignInUiState
-    data class Error(val exception: Throwable) : SignInUiState
-    object BadRequest : SignInUiState
-    object NotFound : SignInUiState
-    object EmailNotValid : SignInUiState
-    object PasswordValid : SignInUiState
+    data class Error(
+        val exception: Throwable? = null,
+        val messageResId: Int,
+        val errorType: ErrorType
+    ) : SignInUiState
+
+    enum class ErrorType {
+        PASSWORD, NOT_FOUND, BAD_REQUEST, SERVER, GENERAL, FORBIDDEN
+    }
 }

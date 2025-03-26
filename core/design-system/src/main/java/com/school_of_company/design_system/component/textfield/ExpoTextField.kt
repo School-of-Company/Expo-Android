e43032459759290dd83496c31aa57f5b337c0622
명훈 <s23065@gsm.hs.kr>
2025-03-26 12:26:38 +0900

    :lipstick: :: Changed RoundedCornerShape Size(6 -> 8)

--- core/design-system/src/main/java/com/school_of_company/design_system/component/textfield/ExpoTextField.kt
+++ core/design-system/src/main/java/com/school_of_company/design_system/component/textfield/ExpoTextField.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -396,7 +397,7 @@ fun ExpoLocationIconTextField(
     onButtonClicked: () -> Unit,
 ) {
     ExpoAndroidTheme { colors, typography ->
-        Box {
+        Box(modifier = modifier.clip(RoundedCornerShape(8.dp))) {
             OutlinedTextField(
                 placeholder = {
                     Text(
@@ -413,7 +414,7 @@ fun ExpoLocationIconTextField(
                     .height(50.dp)
                     .border(
                         width = 1.dp,
-                        shape = RoundedCornerShape(6.dp),
+                        shape = RoundedCornerShape(8.dp),
                         color = colors.gray100
                     )
                     .background(color = Color.Transparent)
@@ -585,6 +586,14 @@ fun ExpoOutlinedTextFieldPreview() {
                 placeholder = "",
                 updateTextValue = {},
                 textStyle = ExpoTypography.bodyBold2.copy(fontWeight = FontWeight.W600)
+            )
+
+            ExpoLocationIconTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = "",
+                isDisabled = false,
+                onButtonClicked = {}
             )
         }
     }

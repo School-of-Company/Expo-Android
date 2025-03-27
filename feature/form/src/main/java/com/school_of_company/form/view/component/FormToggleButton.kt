61903706c0dab4514edf64ba1b9f9b79eed5600f
answad <mymhj122@gmail.com>
2025-03-27 15:24:18 +0900

    :recycle: :: isOverHalf가 true가 아닐때 thumbOffset을 maxBound, minBound로 변경하도록 수정

--- feature/form/src/main/java/com/school_of_company/form/view/component/FormToggleButton.kt
+++ feature/form/src/main/java/com/school_of_company/form/view/component/FormToggleButton.kt
@@ -7,15 +7,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -59,6 +56,7 @@ internal fun FormToggleButton(
                 else thumbOffset > (maxBound / 2)
 
                 if (isOverHalf) onClick()
+                thumbOffset = if (check) maxBound else minBound
             }
         }
 

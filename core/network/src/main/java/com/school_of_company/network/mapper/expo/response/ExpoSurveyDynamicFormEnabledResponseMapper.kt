4abec0baed0a18e60be74db037f069f727d65fba
명훈 <s23065@gsm.hs.kr>
2025-04-10 21:02:49 +0900

    :sparkles: :: Create ExpoSurveyDynamicFormEnableResposneMapper

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/expo/response/ExpoSurveyDynamicFormEnabledResponseMapper.kt
@@ -0,0 +1,20 @@
+package com.school_of_company.network.mapper.expo.response
+
+import com.school_of_company.model.entity.expo.ExpoSurveyDynamicFormEnabledEntity
+import com.school_of_company.model.entity.expo.ExpoValidityEntity
+import com.school_of_company.network.dto.expo.response.ExpoSurveyDynamicFormEnabledResponse
+import com.school_of_company.network.dto.expo.response.ExpoValidityResponse
+
+fun ExpoSurveyDynamicFormEnabledResponse.toEntity(): ExpoSurveyDynamicFormEnabledEntity =
+    ExpoSurveyDynamicFormEnabledEntity(
+        expoValid = this.expoValid.map { it.toEntity() }
+    )
+
+fun ExpoValidityResponse.toEntity(): ExpoValidityEntity =
+    ExpoValidityEntity(
+        expoId = this.expoId,
+        standardFormCreatedStatus = this.standardFormCreatedStatus,
+        traineeFormCreatedStatus = this.traineeFormCreatedStatus,
+        standardSurveyCreatedStatus = this.standardSurveyCreatedStatus,
+        traineeSurveyCreatedStatus = this.traineeSurveyCreatedStatus
+    )

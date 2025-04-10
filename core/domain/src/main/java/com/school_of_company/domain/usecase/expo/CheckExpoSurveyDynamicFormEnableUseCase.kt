a3fa4086be0461e7cb4d337bf92f3ff4e01e2907
명훈 <s23065@gsm.hs.kr>
2025-04-10 21:16:40 +0900

    :sparkles: :: Create CheckExpoSurveyDynamicFormEnableUseCase

---
+++ core/domain/src/main/java/com/school_of_company/domain/usecase/expo/CheckExpoSurveyDynamicFormEnableUseCase.kt
@@ -0,0 +1,13 @@
+package com.school_of_company.domain.usecase.expo
+
+import com.school_of_company.data.repository.expo.ExpoRepository
+import com.school_of_company.model.entity.expo.ExpoSurveyDynamicFormEnabledEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CheckExpoSurveyDynamicFormEnableUseCase @Inject constructor(
+    private val repository: ExpoRepository
+) {
+    operator fun invoke(expoId: String): Flow<ExpoSurveyDynamicFormEnabledEntity> =
+        repository.checkExpoSurveyDynamicFormEnable(expoId = expoId)
+}

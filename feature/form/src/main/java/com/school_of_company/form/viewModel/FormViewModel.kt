0fdbc7f59004b7f89a8f7cbaedd298087a7840a2
answad <mymhj122@gmail.com>
2025-04-11 10:13:55 +0900

    :recycle: :: formState와 informationTextState의 상태를 savedStateHandle로 관리하도록 수정

--- feature/form/src/main/java/com/school_of_company/form/viewModel/FormViewModel.kt
+++ feature/form/src/main/java/com/school_of_company/form/viewModel/FormViewModel.kt
@@ -1,5 +1,6 @@
 package com.school_of_company.form.viewModel
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.school_of_company.common.result.Result
@@ -24,18 +25,22 @@ internal class FormViewModel @Inject constructor(
     private val createFormUseCase: CreateFormUseCase,
     private val getFormUseCase: GetFormUseCase,
     private val modifyFormUseCase: ModifyFormUseCase,
+    private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
-    private val _formState = MutableStateFlow<List<DynamicFormModel>>(listOf(DynamicFormModel.createDefault()))
-    internal val formState = _formState.asStateFlow()
 
-    private val _informationTextState = MutableStateFlow("")
-    internal val informationTextState = _informationTextState.asStateFlow()
+    companion object {
+        private const val FORM_STATE = "form_state"
+        private const val INFORMATION_TEXT_STATE = "information_text_state"
+    }
 
     private val _formUiState = MutableStateFlow<FormUiState>(FormUiState.Loading)
     internal val formUiState = _formUiState.asStateFlow()
 
     private val _getFormUiState = MutableStateFlow<GetFormUiState>(GetFormUiState.Loading)
     internal val getFormUiState = _getFormUiState.asStateFlow()
+
+    internal val formState = savedStateHandle.getStateFlow(FORM_STATE, listOf(DynamicFormModel.createDefault()))
+    internal val informationTextState = savedStateHandle.getStateFlow(INFORMATION_TEXT_STATE, "")
 
     internal fun createForm(
         expoId: String,
@@ -47,7 +52,7 @@ internal class FormViewModel @Inject constructor(
             expoId = expoId,
             body = FormRequestAndResponseModel(
                 participantType = participantType,
-                dynamicForm = _formState.value,
+                dynamicForm = formState.value,
                 informationText = informationText
             ),
         )
@@ -73,7 +78,7 @@ internal class FormViewModel @Inject constructor(
             expoId = expoId,
             body = FormRequestAndResponseModel(
                 participantType = participantType,
-                dynamicForm = _formState.value,
+                dynamicForm = formState.value,
                 informationText = informationText
             ),
         )
@@ -104,35 +109,37 @@ internal class FormViewModel @Inject constructor(
                     is Result.Loading -> _getFormUiState.value = GetFormUiState.Loading
                     is Result.Success -> with(result.data) {
                         _getFormUiState.value = GetFormUiState.Success
-                        _formState.value = dynamicForm
-                        _informationTextState.value = informationText
+                        savedStateHandle[FORM_STATE] = dynamicForm
+                        savedStateHandle[INFORMATION_TEXT_STATE] = informationText
                     }
-                    is Result.Error -> _getFormUiState.value = GetFormUiState.Error(result.exception)
+
+                    is Result.Error -> _getFormUiState.value =
+                        GetFormUiState.Error(result.exception)
                 }
             }
     }
 
     internal fun updateDynamicFormItem(index: Int, newItem: DynamicFormModel) {
-        val currentList = _formState.value.toMutableList()
+        val currentList = formState.value.toMutableList()
         if (index in currentList.indices) {
             currentList[index] = newItem
-            _formState.value = currentList
+            savedStateHandle[FORM_STATE] = currentList
         }
     }
 
     internal fun removeDynamicFormItem(index: Int) {
-        val currentList = _formState.value.toMutableList()
+        val currentList = formState.value.toMutableList()
         if (index in currentList.indices) {
             currentList.removeAt(index)
-            _formState.value = currentList
+            savedStateHandle[FORM_STATE] = currentList
         }
     }
 
     internal fun addEmptyDynamicFormItem() {
-        _formState.value += DynamicFormModel.createDefault()
+        savedStateHandle[FORM_STATE] = formState.value + DynamicFormModel.createDefault()
     }
 
     internal fun setInformationTextState(value: String) {
-        _informationTextState.value = value
+        savedStateHandle[INFORMATION_TEXT_STATE] = value
     }
 }

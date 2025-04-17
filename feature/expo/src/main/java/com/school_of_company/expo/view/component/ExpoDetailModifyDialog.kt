9e37d809a3f408e50c7f0b1978f0abc5b79dbf78
명훈 <s23065@gsm.hs.kr>
2025-04-17 10:59:16 +0900

    :fire: :: Fire Unused Import Code - ExpoDetailModifyDialog

--- feature/expo/src/main/java/com/school_of_company/expo/view/component/ExpoDetailModifyDialog.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/view/component/ExpoDetailModifyDialog.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable

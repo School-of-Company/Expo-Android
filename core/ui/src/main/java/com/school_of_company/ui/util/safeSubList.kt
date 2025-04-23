57e816cbdd07ffcff9f844afe0d53e7127f3d9c2
answad <mymhj122@gmail.com>
2025-04-23 18:02:12 +0900

    :fire: :: safeSubList 삭제

--- core/ui/src/main/java/com/school_of_company/ui/util/safeSubList.kt
+++
@@ -1,7 +0,0 @@
-package com.school_of_company.ui.util
-
-fun <T> List<T>.safeSubList(fromIndex: Int, toIndex: Int): List<T> {
-    val start = fromIndex.coerceIn(0, size)
-    val end = toIndex.coerceIn(start, size)
-    return subList(start, end)
-}

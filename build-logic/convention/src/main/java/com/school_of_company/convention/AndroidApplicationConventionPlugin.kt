599307461bd7db6ae1ad52c8c2d1360a920c3cfa
명훈 <s23065@gsm.hs.kr>
2025-04-28 12:22:37 +0900

    :memo: :: Upgrade Version(Code, Name)

--- build-logic/convention/src/main/java/com/school_of_company/convention/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/school_of_company/convention/AndroidApplicationConventionPlugin.kt
@@ -34,8 +34,8 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                     applicationId = "com.school_of_company.expo_android"
                     minSdk = 26
                     targetSdk = 34
-                    versionCode = 20250426
-                    versionName = "1.2"
+                    versionCode = 20250428
+                    versionName = "1.2.1"
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
                     vectorDrawables.useSupportLibrary = true

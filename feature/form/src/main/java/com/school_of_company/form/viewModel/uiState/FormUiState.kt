ddd2a20d42681d0c3881a6835572f68859b9ae6a
명훈 <s23065@gsm.hs.kr>
2025-04-30 10:50:24 +0900

    :recycle: :: Add Conflict Error In FormUiState

--- feature/form/src/main/java/com/school_of_company/form/viewModel/uiState/FormUiState.kt
+++ feature/form/src/main/java/com/school_of_company/form/viewModel/uiState/FormUiState.kt
@@ -4,5 +4,6 @@ internal sealed interface FormUiState {
     object Loading : FormUiState
     object Success : FormUiState
     object NotFound : FormUiState
+    object Conflict : FormUiState
     data class Error(val exception: Throwable) : FormUiState
 }

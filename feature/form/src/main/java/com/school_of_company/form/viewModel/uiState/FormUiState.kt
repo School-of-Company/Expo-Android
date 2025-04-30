caae70bfa926358f2daf856af3df2b36d9aa239e
명훈 <s23065@gsm.hs.kr>
2025-04-30 10:12:49 +0900

    :recycle: :: Add Not Found Error State In FormUiState

--- feature/form/src/main/java/com/school_of_company/form/viewModel/uiState/FormUiState.kt
+++ feature/form/src/main/java/com/school_of_company/form/viewModel/uiState/FormUiState.kt
@@ -3,5 +3,6 @@ package com.school_of_company.form.viewModel.uiState
 internal sealed interface FormUiState {
     object Loading : FormUiState
     object Success : FormUiState
+    object NotFound : FormUiState
     data class Error(val exception: Throwable) : FormUiState
 }

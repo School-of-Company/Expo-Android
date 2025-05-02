c6b29cb56faea56e9b4328c4b579769e012fbbec
youloveli <mymhj122@gmail.com>
2025-05-02 10:15:38 +0900

    :recycle: :: AndroidFeatureConven 또한 참조하도록 수정 tionPlugin이 core:data

--- build-logic/convention/src/main/java/com/school_of_company/convention/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/school_of_company/convention/AndroidFeatureConventionPlugin.kt
@@ -25,6 +25,7 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 add("implementation", project(":core:ui"))
                 add("implementation", project(":core:design-system"))
                 add("implementation", project(":core:domain"))
+                add("implementation", project(":core:data"))
 
                 add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())  // Add Lifecycle runtime for Compose
                 add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())  // Add ViewModel support for Compose

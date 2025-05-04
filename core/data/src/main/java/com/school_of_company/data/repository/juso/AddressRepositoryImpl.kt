5bfc14c8e4796918c4a4d3ea7487fe64a57bed1f
youloveli <mymhj122@gmail.com>
2025-05-04 13:29:54 +0900

    :fire: :: GetAddressUseCase 삭제

--- core/data/src/main/java/com/school_of_company/data/repository/juso/AddressRepositoryImpl.kt
+++ core/data/src/main/java/com/school_of_company/data/repository/juso/AddressRepositoryImpl.kt
@@ -11,14 +11,17 @@ class AddressRepositoryImpl @Inject constructor(
     private val addressDataSource: AddressDataSource,
 ) : AddressRepository {
 
+    companion object {
+        private const val DEFAULT_PAGE = 1
+        private const val DEFAULT_PAGE_SIZE = 5
+    }
+
     override fun getAddress(
-        currentPage: Int,
-        countPerPage: Int,
         keyword: String
     ): Flow<List<JusoModel>> =
         addressDataSource.getAddress(
-            countPerPage = countPerPage,
-            currentPage = currentPage,
+            countPerPage = DEFAULT_PAGE_SIZE,
+            currentPage = DEFAULT_PAGE,
             keyword = keyword
         ).transform { list ->
             emit(list.results.juso?.map { juso -> juso.toModel() } ?: emptyList())
--- feature/expo/src/main/java/com/school_of_company/expo/viewmodel/ExpoViewModel.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/viewmodel/ExpoViewModel.kt
@@ -9,8 +9,8 @@ import com.school_of_company.common.exception.NoResponseException
 import com.school_of_company.common.result.Result
 import com.school_of_company.common.result.asResult
 import com.school_of_company.data.repository.expo.ExpoRepository
+import com.school_of_company.data.repository.juso.AddressRepository
 import com.school_of_company.domain.usecase.Image.ImageUpLoadUseCase
-import com.school_of_company.domain.usecase.juso.GetAddressUseCase
 import com.school_of_company.domain.usecase.kakao.GetCoordinatesToAddressUseCase
 import com.school_of_company.domain.usecase.kakao.GetCoordinatesUseCase
 import com.school_of_company.domain.usecase.standard.StandardProgramListUseCase
@@ -55,7 +55,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 internal class ExpoViewModel @Inject constructor(
-    private val getAddressUseCase: GetAddressUseCase,
+    private val addressRepository: AddressRepository,
     private val expoRepository: ExpoRepository,
     private val imageUpLoadUseCase: ImageUpLoadUseCase,
     private val getCoordinatesUseCase: GetCoordinatesUseCase,
@@ -464,7 +464,7 @@ internal class ExpoViewModel @Inject constructor(
     internal fun searchLocation(searchText: String) =
         viewModelScope.launch {
             onSearchedCoordinateChange(x = "", y = "")
-            getAddressUseCase(searchText = searchText)
+            addressRepository.getAddress(keyword = searchText)
                 .asResult()
                 .collectLatest { result ->
                     when (result) {

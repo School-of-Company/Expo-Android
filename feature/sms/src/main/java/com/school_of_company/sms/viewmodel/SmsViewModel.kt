b6c64119c3006bd4c67ffe0f19778b39c8bfbd6e
명훈 <s23065@gsm.hs.kr>
2025-05-06 05:08:24 +0900

    :recycle: :: Replace Anemic Domain With Repository In SmsViewModel

--- feature/sms/src/main/java/com/school_of_company/sms/viewmodel/SmsViewModel.kt
+++ feature/sms/src/main/java/com/school_of_company/sms/viewmodel/SmsViewModel.kt
@@ -3,19 +3,21 @@ package com.school_of_company.sms.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.school_of_company.domain.usecase.sms.SendSmsToParticipantTraineeUseCase
+import com.school_of_company.common.result.Result
+import com.school_of_company.common.result.asResult
+import com.school_of_company.data.repository.sms.SmsRepository
 import com.school_of_company.model.param.sms.SendSmsToParticipantTraineeParam
 import com.school_of_company.sms.viewmodel.uiState.SendSmsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 internal class SmsViewModel @Inject constructor(
-    private val sendSmsToParticipantTraineeUseCase: SendSmsToParticipantTraineeUseCase,
+    private val smsRepository: SmsRepository,
     private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
     companion object {
@@ -33,16 +35,17 @@ internal class SmsViewModel @Inject constructor(
         id: String,
         body: SendSmsToParticipantTraineeParam
     ) = viewModelScope.launch {
-        sendSmsToParticipantTraineeUseCase(id, body)
-            .onSuccess {
-                it.catch { remoteError ->
-                    _sendSmsUiState.value = SendSmsUiState.Error(remoteError)
-                }.collect {
-                    _sendSmsUiState.value = SendSmsUiState.Success
+        smsRepository.sendSmsToParticipantTrainee(
+            id = id,
+            body = body
+        )
+            .asResult()
+            .collectLatest { result ->
+                when (result) {
+                    is Result.Loading -> _sendSmsUiState.value = SendSmsUiState.Loading
+                    is Result.Success -> _sendSmsUiState.value = SendSmsUiState.Success
+                    is Result.Error -> _sendSmsUiState.value = SendSmsUiState.Error(result.exception)
                 }
-            }
-            .onFailure { error ->
-                _sendSmsUiState.value = SendSmsUiState.Error(error)
             }
     }
 

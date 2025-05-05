c025085f3e5a4e7882c6c318910bd218c6fbf4dd
명훈 <s23065@gsm.hs.kr>
2025-05-06 01:04:39 +0900

    :fire: :: Fire Unused Import Code - ExpoCreatedScreen

--- feature/expo/src/main/java/com/school_of_company/expo/view/ExpoCreatedScreen.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/view/ExpoCreatedScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle

b675bd41cbc4357d668a48355589a62db82f7c63
youloveli <mymhj122@gmail.com>
2025-05-14 12:14:16 +0900

    :recycle: :: DynamicFormModel를 Stable 하다고 인식하도록 Immutable 어노테이션 추가

--- core/model/src/main/java/com/school_of_company/model/model/form/FormRequestAndResponseModel.kt
+++ core/model/src/main/java/com/school_of_company/model/model/form/FormRequestAndResponseModel.kt
@@ -1,11 +1,15 @@
 package com.school_of_company.model.model.form
 
+import androidx.compose.runtime.Immutable
+
 data class FormRequestAndResponseModel(
     val informationText: String,
     val participantType: String, // TRAINEE, STANDARD
     val dynamicForm: List<DynamicFormModel>,
 )
 
+
+@Immutable
 data class DynamicFormModel(
     val title: String,
     val formType: String, // SENTENCE, CHECKBOX, DROPDOWN, IMAGE, MULTIPLE

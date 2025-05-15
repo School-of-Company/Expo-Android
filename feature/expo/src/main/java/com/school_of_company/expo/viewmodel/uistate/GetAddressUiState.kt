99b43c2f975b444cba86914122342f065a761424
명훈 <s23065@gsm.hs.kr>
2025-05-15 16:57:39 +0900

    :recycle: :: Apply Reviewer - ImmutableList -> PersistentList

--- feature/expo/src/main/java/com/school_of_company/expo/viewmodel/uistate/GetAddressUiState.kt
+++ feature/expo/src/main/java/com/school_of_company/expo/viewmodel/uistate/GetAddressUiState.kt
@@ -1,10 +1,10 @@
 package com.school_of_company.expo.viewmodel.uistate
 
 import com.school_of_company.model.model.juso.JusoModel
-import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.PersistentList
 
 sealed interface GetAddressUiState {
     object Loading : GetAddressUiState
-    data class Success(val data: ImmutableList<JusoModel>) : GetAddressUiState
+    data class Success(val data: PersistentList<JusoModel>) : GetAddressUiState
     data class Error(val exception: Throwable) : GetAddressUiState
 }
